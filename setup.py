9b2c0eb97f02e4ec243a26f05b1911861ca8306e
rlafuente@pathdatapatata <none@none>
2008-08-26 09:26:46 +0100

    remaining Shoebox->bot changes; fixed examples/ location in setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ from distutils.core import setup
 
 setup(name = "shoebot",
       version = "0.2",
-      description = "Shoebot - a Pythonic vector graphics scripting application",
+      description = "Shoebot - a vector graphics scripting application",
       author = "ricardo lafuente",
       author_email = "r@sollec.org",
       license = 'GPL v3',
       url = "http://shoebot.sollec.org",
       packages = ["shoebot",
                   ],
-      data_files = [("share/examples", ["examples/primitives.py",
+      data_files = [("share/shoebot/examples", ["examples/primitives.py",
                                         "examples/socketcontrol.pd",
                                         "examples/socketcontrol.py",
                                         "examples/socketcontrol2.py",
                                         ])
                     ],
-      scripts = ["sbox"],
+      scripts = ["sbot"],
       long_description = """
 Shoebot is a runtime for reading Nodebox source files. It
 implements Nodebox's domain-specific language through Cairo.
