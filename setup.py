ac7094fb9d76f7db82d9766bd48e7c135f272266
Ricardo Lafuente <r@sollec.org>
2008-09-18 18:19:39 +0100

    updated setup.py: globbed all examples into a generator, but still has problems (doesn't create any subdirs)

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
 
 setup(name = "shoebot",
-      version = "0.2",
+      version = "0.1",
       description = "A vector graphics scripting application",
-      author = "ricardo lafuente",
+      author = "Ricardo Lafuente",
       author_email = "r@sollec.org",
       license = 'GPL v3',
       url = "http://shoebot.sollec.org",
       packages = ["shoebot",
                   ],
-      data_files = [("share/shoebot/examples", ["examples/primitives.bot",
-                                  "examples/socketcontrol.pd",
-                                        "examples/socketcontrol.bot",
-                                        "examples/socketcontrol2.bot",
-                                  ])
-                    ],
+      data_files = [("share/shoebot/examples",
+                     [os.path.join(root, file_) for file_ in files])
+                  for root, dirs, files in os.walk('examples')],
       scripts = ["sbot", "shoebot-ide"],
       long_description = """
-Shoebot is a Python application for vector scripting.
-It implements some shortcuts around the intricacies of Cairo,
-and can also run external scripts and quickly generate image
-outputs, as well as operating in realtime in windowed (GTK) mode.
+ Shoebot is a pure Python graphics robot: It takes a Python script as input,
+ which describes a drawing process, and outputs a graphic in a common open
+ standard format (SVG, PDF, PostScript, or PNG). It has a simple text editor
+ GUI, and scripts can describe their own GUIs for controlling variables
+ interactively. Being pure Python, it can also be used as a Python module,
+ a plugin for Python-scriptable tools such as Inkscape, and run from the
+ command line.
+
 """
 )
 
