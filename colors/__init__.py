7712080885bcb42ffd65bacc8cd18258c1d3e0a0
francesco <none@none>
2008-11-07 14:34:55 +0100

    minor changes to colors library in order to make swarm() method to work. still there seem to be some issues with CORNER mode transforms

--- colors/__init__.py
+++ colors/__init__.py
@@ -1269,23 +1269,27 @@ class ColorList(_list):
         """ Fancy random ovals for all the colors in the list.
         """
         
-        sc = _ctx.stroke()
-        sw = _ctx.strokewidth()
+        sc = _ctx.stroke(0,0,0,0)
+        sw = _ctx.strokewidth(0)
         
-        _ctx.push()
-        _ctx.transform(CORNER)
+        _ctx.push()        
+        _ctx.transform(_ctx.CORNER)
         _ctx.translate(x, y)
+
         for i in _range(r*3):
+            
             clr = choice(self).copy()
             clr.alpha -= 0.5 * random()
             _ctx.fill(clr)
             clr = choice(self)
             _ctx.stroke(clr)
             _ctx.strokewidth(10 * random())
+            
             _ctx.rotate(360 * random())
+            
             r2 = r*0.5 * random()
             _ctx.oval(r*random(), 0, r2, r2)
-        _ctx.reset()
+        _ctx.pop()
         
         _ctx.strokewidth(sw)
         if sc == None: 
