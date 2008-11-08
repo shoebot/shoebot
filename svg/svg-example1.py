68639e0c0621ae3219937197f5230f40453f0c10
francesco <none@none>
2008-11-08 02:18:02 +0100

    minor modifications t example scripts

--- svg/svg-example1.py
+++ svg/svg-example1.py
@@ -12,9 +12,9 @@ for i in range(10):
     
     transform(mode=CORNER)
     
-    translate(random(50, 200), random(50, 200))
+    translate(random(0, 200), random(0, 200))
     scale(random(0.4, 1.5))
-    rotate(degrees=random(60))
+    rotate(degrees=random(360))
 
     fill(1,0.2,0.5,0.1)
 
