c258c6c6b4464d021276a23117c77852d41b7fa5
Ricardo Lafuente <r@sollec.org>
2009-06-20 22:42:47 +0530

    refactored the Cairo canvas and path transform handling; paths now don't have their own transforms, and grobs get passed directly from Bot to Canvas without use of a grob stack. Then the canvas reads the bot's current transform values. While this breaks stuff like rect(x,y,w,h,rotate=20), which is easily fixed, animation looks at least 2x faster on my pc :) also, a lot of cruft cleaned on the way.

--- shoebot/core/cairo_canvas.py
+++ shoebot/core/cairo_canvas.py
@@ -3,10 +3,9 @@ import cairo
 from math import pi
 # FIXME: we ought to make relative imports, but only when Cairocanvas is not called
 # from BezierPath
-import shoebot
 from shoebot import ShoebotError
 from shoebot.core import Canvas
-
+from shoebot.data import BezierPath, ClippingPath, RestoreCtx, Image, Text
 
 import locale, gettext
 APP = 'shoebot'
@@ -27,50 +26,17 @@ ARC = 'arc'
 ELLIPSE = 'ellipse'
 CLOSE = "close"
 
+CENTER = 'center'
+CORNER = 'corner'
+
 class CairoCanvas(Canvas):    
    
     def __init__(self, bot=None, target=None, width=None, height=None, gtkmode=False):
         Canvas.__init__(self, bot, target, width, height, gtkmode)
-
         self.bot = bot
-
         if not gtkmode:
             # image output mode, we need to make a surface
             self.setsurface(target, width, height)
-
-    def setsurface(self, target=None, width=None, height=None):
-        '''Sets the surface on which the Canvas object will operate.
-
-        Besides attaching surfaces, it can also create new ones based on an
-        output filename; it also accepts a Cairo surface or context as an
-        argument, and attaches to them as expected.
-        '''
-
-        if not target:
-            raise ShoebotError(_("setsurface(): No target specified!"))
-        if isinstance(target, basestring):
-            # if the target is a string, should be a filename
-            filename = target
-            if not width:
-                width = self.WIDTH
-            if not height:
-                height = self.HEIGHT
-            self._surface = surfacefromfilename(filename,width,height)
-            self._context = cairo.Context(self._surface)
-        elif isinstance(target, cairo.Surface):
-            # and if it's a surface, attach our Cairo context to it
-            self._surface = target
-            self._context = cairo.Context(self._surface)
-        elif isinstance(target, cairo.Context):
-            # if it's a Cairo context, use it instead of making a new one
-            self._context = target
-            self._surface = self._context.get_target()
-        else:
-            raise ShoebotError(_("setsurface: Argument must be a file name, a Cairo surface or a Cairo context"))
-
-    def draw(self, ctx=None):
-        if not ctx:
-            ctx = self._context
 
         # following block checks if we are in gtkmode and in case of fullscreen it scales cairo context
         # and centers it on screen
@@ -85,127 +51,108 @@ class CairoCanvas(Canvas):
                     self.fullscreen_scale =  float(self.bot.screen_height) / float(self.bot.HEIGHT)
                     self.fullscreen_deltax = (self.bot.screen_width - (self.bot.WIDTH*self.fullscreen_scale))/2
                     self.fullscreen_deltay = 0
-                ctx.translate(self.fullscreen_deltax, self.fullscreen_deltay)
-                ctx.scale(self.fullscreen_scale, self.fullscreen_scale)
+                self.context.translate(self.fullscreen_deltax, self.fullscreen_deltay)
+                self.context.scale(self.fullscreen_scale, self.fullscreen_scale)
 
-        # draws things
-        for item in self.grobstack:
-            if isinstance(item, shoebot.data.ClippingPath):
-                ctx.save()
-                ctx.save()
-                deltax, deltay = item.center
-                m = item._transform.get_matrix_with_center(deltax,deltay,item._transformmode)
-                ctx.transform(m)
-                self.drawclip(item, ctx)
-            elif isinstance(item, shoebot.data.RestoreCtx):
-                ctx.restore()
-            else:
-                if isinstance(item, shoebot.data.BezierPath):
-                    ctx.save()
-                    deltax, deltay = item.center
-                    m = item._transform.get_matrix_with_center(deltax,deltay,item._transformmode)
-                    ctx.transform(m)
-                    self.drawpath(item, ctx)
-                elif isinstance(item, shoebot.data.Text):
-                    ctx.save()
-                    x,y = item.metrics[0:2]
-                    deltax, deltay = item.center
-                    m = item._transform.get_matrix_with_center(deltax,deltay-item.baseline,item._transformmode)
-                    ctx.transform(m)
-                    ctx.translate(item.x,item.y-item.baseline)
-                    self.drawtext(item, ctx)
-                elif isinstance(item, shoebot.data.Image):
-                    ctx.save()
-                    deltax, deltay = item.center
-                    m = item._transform.get_matrix_with_center(deltax,deltay,item._transformmode)
-                    ctx.transform(m)
-                    self.drawimage(item, ctx)
+    def drawitem(self, item):
+        # clips are special cases
+        if isinstance(item, RestoreCtx):
+            self.context.restore()
+            return
+        if isinstance(item, ClippingPath):
+            self.context.save()
+            
+        if isinstance(item,Text):
+            deltay -= item.baseline
+            
+        # get the bot's current transform matrix and apply it to the context
+        self.context.save()
+        if self.bot._transformmode == CORNER:
+            deltax, deltay = (0,0)
+        else:
+            deltax, deltay = item.center
+        mtrx = self.bot._transform.get_matrix_with_center(deltax,deltay)
+        self.context.transform(mtrx)
+        
+        # find item type and call appropriate drawing method
+        if isinstance(item, ClippingPath):
+            self.drawclip(item)
+        elif isinstance(item, BezierPath):
+            self.drawpath(item)
+        elif isinstance(item, Text):
+            self.drawtext(item)
+        elif isinstance(item, Image):
+            self.drawimage(item)
+            
+        self.context.restore()
 
-                ctx.restore()
-
-    def drawclip(self,path,ctx=None):
-        '''Passes the path to a Cairo context.'''
-        if not isinstance(path, shoebot.data.ClippingPath):
-            raise shoebot.ShoebotError(_("drawpath(): Expecting a ClippingPath, got %s") % (path))
-
-        if not ctx:
-            ctx = self._context
-
+    def drawclip(self,path):
         for element in path.data:
             cmd = element[0]
             values = element[1:]
 
             # apply cairo context commands
             if cmd == MOVETO:
-                ctx.move_to(*values)
+                self.context.move_to(*values)
             elif cmd == LINETO:
-                ctx.line_to(*values)
+                self.context.line_to(*values)
             elif cmd == CURVETO:
-                ctx.curve_to(*values)
+                self.context.curve_to(*values)
             elif cmd == RLINETO:
-                ctx.rel_line_to(*values)
+                self.context.rel_line_to(*values)
             elif cmd == RCURVETO:
-                ctx.rel_curve_to(*values)
+                self.context.rel_curve_to(*values)
             elif cmd == CLOSE:
-                ctx.close_path()
+                self.context.close_path()
             elif cmd == ELLIPSE:
                 x, y, w, h = values
-                ctx.save()
-                ctx.translate (x + w / 2., y + h / 2.)
-                ctx.scale (w / 2., h / 2.)
-                ctx.arc (0., 0., 1., 0., 2 * pi)
-                ctx.restore()
+                self.context.save()
+                self.context.translate (x + w / 2., y + h / 2.)
+                self.context.scale (w / 2., h / 2.)
+                self.context.arc (0., 0., 1., 0., 2 * pi)
+                self.context.restore()
             else:
                 raise ShoebotError(_("PathElement(): error parsing path element command (got '%s')") % (cmd))
-        ctx.restore()
-        ctx.clip()
+        self.context.restore()
+        self.context.clip()
 
 
-    def drawtext(self,txt,ctx=None):
-        if not ctx:
-            ctx = self._context
+    def drawtext(self,txt):
         if txt._fillcolor:
-            ctx.set_source_rgba(*txt._fillcolor)
+            self.context.set_source_rgba(*txt._fillcolor)
             if txt._strokecolor:
                 # if there's a stroke still to be applied, we need to call fill_preserve()
                 # which still leaves this path as active
-                ctx.fill_preserve()
-                ctx.set_source_rgba(*txt._strokecolor)
+                self.context.fill_preserve()
+                self.context.set_source_rgba(*txt._strokecolor)
                 # now apply the stroke (stroke ends the path, we'd use stroke_preserve()
                 # for further operations if needed)
-                ctx.stroke()
+                self.context.stroke()
             else:
                 # if there isn't a stroke, use plain fill() to close the path
-                ctx.fill()
+                self.context.fill()
         elif txt._strokecolor:
             # if there's no fill, apply stroke only
-            ctx.set_source_rgba(*txt._strokecolor)
-            ctx.stroke()
+            self.context.set_source_rgba(*txt._strokecolor)
+            self.context.stroke()
         else:
             print _("Warning: Canvas object had no fill or stroke values")
 
-        txt.pang_ctx.update_layout(txt.layout)
-        txt.pang_ctx.show_layout(txt.layout)
+        txt.pang_self.context.update_layout(txt.layout)
+        txt.pang_self.context.show_layout(txt.layout)
 
 
 
-    def drawimage(self,image,ctx=None):
-        if not ctx:
-            ctx = self._context
-        ctx.set_source_surface (image.imagesurface, image.x, image.y)
-        ctx.rectangle(image.x, image.y, image.width, image.height)
-        ctx.fill()
+    def drawimage(self,image):
+        self.context.set_source_surface (image.imagesurface, image.x, image.y)
+        self.context.rectangle(image.x, image.y, image.width, image.height)
+        self.context.fill()
 
-    def drawpath(self,path,ctx=None):
+    def drawpath(self,path):
         '''Passes the path to a Cairo context.'''
-        if not isinstance(path, shoebot.data.BezierPath):
-            raise ShoebotError(_("drawpath(): Expecting a BezierPath, got %s") % (path))
-
-        if not ctx:
-            ctx = self._context
 
         if path._strokewidth:
-            ctx.set_line_width(path._strokewidth)
+            self.context.set_line_width(path._strokewidth)
 
         for element in path.data:
             cmd = element[0]
@@ -213,52 +160,52 @@ class CairoCanvas(Canvas):
 
             # apply cairo context commands
             if cmd == MOVETO:
-                ctx.move_to(*values)
+                self.context.move_to(*values)
             elif cmd == LINETO:
-                ctx.line_to(*values)
+                self.context.line_to(*values)
             elif cmd == CURVETO:
-                ctx.curve_to(*values)
+                self.context.curve_to(*values)
             elif cmd == RLINETO:
-                ctx.rel_line_to(*values)
+                self.context.rel_line_to(*values)
             elif cmd == RCURVETO:
-                ctx.rel_curve_to(*values)
+                self.context.rel_curve_to(*values)
             elif cmd == ARC:
-                ctx.arc(*values)
+                self.context.arc(*values)
             elif cmd == CLOSE:
-                ctx.close_path()
+                self.context.close_path()
             elif cmd == ELLIPSE:
                 x, y, w, h = values
-                ctx.save()
-                ctx.translate (x + w / 2., y + h / 2.)
-                ctx.scale (w / 2., h / 2.)
-                ctx.arc (0., 0., 1., 0., 2 * pi)
-                ctx.restore()
+                self.context.save()
+                self.context.translate (x + w / 2., y + h / 2.)
+                self.context.scale (w / 2., h / 2.)
+                self.context.arc (0., 0., 1., 0., 2 * pi)
+                self.context.restore()
             else:
                 raise ShoebotError(_("PathElement(): error parsing path element command (got '%s')") % (cmd))
 
         if path._fillcolor:
-            ctx.set_source_rgba(*path._fillcolor)
+            self.context.set_source_rgba(*path._fillcolor)
             if path._strokecolor:
                 # if there's a stroke still to be applied, we need to call fill_preserve()
                 # which still leaves this path as active
-                ctx.fill_preserve()
-                ctx.set_source_rgba(*path._strokecolor)
+                self.context.fill_preserve()
+                self.context.set_source_rgba(*path._strokecolor)
                 # now apply the stroke (stroke ends the path, we'd use stroke_preserve()
                 # for further operations if needed)
-                ctx.stroke()
+                self.context.stroke()
             else:
                 # if there isn't a stroke, use plain fill() to close the path
-                ctx.fill()
+                self.context.fill()
         elif path._strokecolor:
             # if there's no fill, apply stroke only
-            ctx.set_source_rgba(*path._strokecolor)
-            ctx.stroke()
+            self.context.set_source_rgba(*path._strokecolor)
+            self.context.stroke()
         else:
             print _("Warning: Canvas object had no fill or stroke values")
 
     def finish(self):
         if isinstance(self._surface, (cairo.SVGSurface, cairo.PSSurface, cairo.PDFSurface)):
-            self._context.show_page()
+            self.context.show_page()
             self._surface.finish()
         else:
             self._surface.write_to_png(self._bot.targetfilename)
@@ -289,11 +236,44 @@ class CairoCanvas(Canvas):
             print _("Saved snapshot to %s") % filename
 
         elif isinstance(target, cairo.Context):
-            ctx = target
-            self.draw(ctx)
+            self.context = target
+            self.draw(self.context)
         elif isinstance(target, cairo.Surface):
-            ctx = target.get_context()
-            self.draw(ctx)
+            self.context = target.get_context()
+            self.draw(self.context)
+
+
+    def setsurface(self, target=None, width=None, height=None):
+        '''Sets the surface on which the Canvas object will operate.
+
+        Besides attaching surfaces, it can also create new ones based on an
+        output filename; it also accepts a Cairo surface or context as an
+        argument, and attaches to them as expected.
+        '''
+
+        if not target:
+            raise ShoebotError(_("setsurface(): No target specified!"))
+        if isinstance(target, basestring):
+            # if the target is a string, should be a filename
+            filename = target
+            if not width:
+                width = self.WIDTH
+            if not height:
+                height = self.HEIGHT
+            self._surface = surfacefromfilename(filename,width,height)
+            self.context = cairo.Context(self._surface)
+        elif isinstance(target, cairo.Surface):
+            # and if it's a surface, attach our Cairo context to it
+            self._surface = target
+            self.context = cairo.Context(self._surface)
+        elif isinstance(target, cairo.Context):
+            # if it's a Cairo context, use it instead of making a new one
+            self.context = target
+            self._surface = self.context.get_target()
+        else:
+            raise ShoebotError(_("setsurface: Argument must be a file name, a Cairo surface or a Cairo context"))
+        
+
 
 
 def surfacefromfilename(outfile, width, height):
