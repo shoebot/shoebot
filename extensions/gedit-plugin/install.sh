2c04452984d0e13f905d3f2dd3a8ee24b03cbf6d
rlafuente@antid <none@none>
2010-01-18 17:12:17 +0000

    improved Gedit install file: fixed copying to overwrite existing files, and added the syntax definition installation as well

--- extensions/gedit-plugin/install.sh
+++ extensions/gedit-plugin/install.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
 if [ -d ~/.gnome2/gedit/plugins ]; then
-  cp shoebotit* ~/.gnome2/gedit/plugins
+  cp -f shoebotit* ~/.gnome2/gedit/plugins
   echo 'Gedit plugin successfully installed!'
 else
   echo 'Local Gedit plugin directory does not exist. Creating it.'
   mkdir -p ~/.gnome2/gedit/plugins
-  cp shoebotit* ~/.gnome2/gedit/plugins
+  cp -f shoebotit* ~/.gnome2/gedit/plugins
   echo 'Gedit plugin successfully installed!'
 fi
+
+if [ -d ~/.gnome2/gtksourceview-1.0/language-specs ]; then
+  cp -f shoebot.lang ~/.gnome2/gtksourceview-1.0/language-specs
+  echo 'GTKSourceView language specs successfully installed!'
+else
+  echo 'Local GTKSourceView language-specs directory does not exist. Creating it.'
+  mkdir -p ~/.gnome2/gtksourceview-1.0/language-specs
+  cp -f shoebot.lang ~/.gnome2/gtksourceview-1.0/language-specs
+  echo 'GTKSourceView language specs successfully installed!'
+fi
+
 
