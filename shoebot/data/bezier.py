e0b4ced9d9497b1b876f90901b6dbc35c4133f04
Stuart Axon, <stu.axon@gmail.com>
2010-05-24 21:39:56 +0100

    Add start of contours functionality from nodebox

--- shoebot/data/bezier.py
+++ shoebot/data/bezier.py
@@ -198,7 +198,44 @@ class BezierPath(Grob):
         self._deferred_render(self._render_closure())
 
     def _get_contours(self):
-        raise NotImplementedError()
+        """ Returns a list of contours in the path, as BezierPath objects.
+            A contour is a sequence of lines and curves separated from the next contour by a MOVETO.
+            For example, the glyph "o" has two contours: the inner circle and the outer circle.
+
+            From nodebox
+        """
+        contours = []
+        current_contour = None
+        empty = True
+        for i, el in enumerate(self._get_elements()):
+            if el.cmd == MOVETO:
+                if not empty:
+                    contours.append(current_contour)
+                current_contour = BezierPath(self._canvas)
+                current_contour.moveto(el.x, el.y)
+                empty = True
+            elif el.cmd == LINETO:
+                empty = False
+                current_contour.lineto(el.x, el.y)
+            elif el.cmd == CURVETO:
+                empty = False
+                current_contour.curveto(el.c1x, el.c1y, el.c2x, el.c2y, el.x, el.y)
+            elif el.cmd == CLOSE:
+                current_contour.closepath()
+        if not empty:
+            contours.append(current_contour)
+        return contours
+
+    def _get_elements(self):
+        '''
+        Yields all elements as PathElements
+        '''
+        for index, el in enumerate(self._elements):
+            if isinstance(el, tuple):
+                el = PathElement(*el)
+                self._elements[index] = el
+            yield el
+
 
     def __getitem__(self, index):
         '''
