40d569b51763da5bfedc60b7690de8ef21df77dd
Stuart Axon, <stu.axon@gmail.com>
2010-05-30 07:42:01 +0100

    Some intitial work on getting right clicks working again

--- shoebot/gui/gtk_window.py
+++ shoebot/gui/gtk_window.py
@@ -1,18 +1,35 @@
+import sys
 import gtk
 
-from shoebot.gui import ShoebotWidget, VarWindow
+from shoebot.gui import ShoebotWidget, VarWindow, SocketServerMixin
 from shoebot.core import DrawQueueSink
 from gtk_input_device import GtkInputDeviceMixin
 
-class ShoebotWindow(gtk.Window, GtkInputDeviceMixin, DrawQueueSink):
+import locale
+import gettext
+
+APP = 'shoebot'
+DIR = sys.prefix + '/share/shoebot/locale'
+locale.setlocale(locale.LC_ALL, '')
+gettext.bindtextdomain(APP, DIR)
+#gettext.bindtextdomain(APP)
+gettext.textdomain(APP)
+_ = gettext.gettext
+
+class ShoebotWindow(gtk.Window, GtkInputDeviceMixin, DrawQueueSink, SocketServerMixin):
     '''Create a GTK+ window that contains a ShoebotWidget'''
 
     # Draw in response to an expose-event
     __gsignals__ = { "expose-event": "override" }
-    def __init__(self, title = None, show_vars = False):
+    def __init__(self, title = None, show_vars = False, server=False, serverport=7777, go_fullscreen=False):
         gtk.Window.__init__(self)
         DrawQueueSink.__init__(self)
         GtkInputDeviceMixin.__init__(self)
+
+        self.has_server = server
+        self.serverport = serverport
+        self.show_vars = show_vars
+        self.var_window = None
 
         sb_widget = ShoebotWidget()
 
@@ -21,6 +38,42 @@ class ShoebotWindow(gtk.Window, GtkInputDeviceMixin, DrawQueueSink):
         self.connect("delete-event", gtk.main_quit)
         self.connect("destroy", self.do_window_close)
 
+        self.sb_widget = sb_widget
+        self.attach_gtk(self)
+
+        self.uimanager = gtk.UIManager()
+        accelgroup = self.uimanager.get_accel_group()
+        self.add_accel_group(accelgroup)
+
+        actiongroup = gtk.ActionGroup('Canvas')
+
+        actiongroup.add_actions([('Save as', None, _('_Save as')),
+                                 ('svg', 'Save as SVG', _('Save as _SVG'), "<Control>1", None, self.snapshot_svg),
+                                 ('pdf', 'Save as PDF', _('Save as _PDF'), "<Control>2", None, self.snapshot_pdf),
+                                 ('ps', 'Save as PS', _('Save as P_S'), "<Control>3", None, self.snapshot_ps),
+                                 ('png', 'Save as PNG', _('Save as P_NG'), "<Control>4", None, self.snapshot_png),
+                                 ('fullscreen', 'Go fullscreen', _('_Go fullscreen'), "<Control>5", None, self.do_fullscreen),
+                                 ('unfullscreen', 'Exit fullscreen', _('_Exit fullscreen'), "<Control>6", None, self.do_unfullscreen),
+                                 ('close', 'Close window', _('_Close Window'), "<Control>w", None, self.do_window_close)
+                                ])
+
+        menuxml = '''
+        <popup action="Save as">
+            <menuitem action="svg"/>
+            <menuitem action="ps"/>
+            <menuitem action="pdf"/>
+            <menuitem action="png"/>
+            <separator/>
+            <menuitem action="fullscreen"/>
+            <menuitem action="unfullscreen"/>
+            <separator/>
+            <menuitem action="close"/>
+        </popup>
+        '''
+
+        self.uimanager.insert_action_group(actiongroup, 0)
+        self.uimanager.add_ui_from_string(menuxml)
+
         sb_widget.show()
         self.add(sb_widget)
 
@@ -28,11 +81,8 @@ class ShoebotWindow(gtk.Window, GtkInputDeviceMixin, DrawQueueSink):
 
         while gtk.events_pending():
             gtk.main_iteration()
-
-        self.show_vars = show_vars
-        self.var_window = None
-        self.sb_widget = sb_widget
-        self.attach_gtk(self)
+        if server:
+            self.server('', self.serverport)
 
 
     def create_rcontext(self, size, frame):
@@ -47,8 +97,49 @@ class ShoebotWindow(gtk.Window, GtkInputDeviceMixin, DrawQueueSink):
 
         return self.sb_widget.rcontext_ready(size, frame, cairo_ctx)
 
+    def snapshot_svg(self):
+        bot = self.bot
+        filename = bot.namespace['__file__'] or 'output' + '.svg'
+        ##self.bot.snapshot(self,filename=None, surface=None, defer=False, autonumber=bot.iterations > 1):
+        ### Should save at end of frame
+        print 'TODO'
+
+    def snapshot_ps(self):
+        print 'TODO'
+
+    def snapshot_pdf(self):
+        print 'TODO'
+
+    def snapshot_png(self):
+        print 'TODO'
+
+    def do_fullscreen(self, widget):
+        self.fullscreen()
+        # next lines seem to be needed for window switching really to
+        # fullscreen mode before reading it's size values
+        while gtk.events_pending():
+            gtk.main_iteration(block=False)
+        # we pass informations on full-screen size to bot
+        #self.bot._screen_width = self.get_allocation().width
+        #self.bot._screen_height = self.get_allocation().height
+        self.bot._screen_width = gtk.gdk.screen_width()
+        self.bot._screen_height = gtk.gdk.screen_height()
+        self.bot._screen_ratio = self.bot.screen_width / self.bot.screen_height
+
+    def do_unfullscreen(self, widget):
+        self.unfullscreen()
+        self.bot._screen_ratio = None
+
     def do_window_close(self, widget):
         self.bot._quit = True
+
+        if self.has_server:
+            self.sock.close()
+        if self.var_window is not None:
+            self.var_window.window.destroy()
+            self.var_window = None
+
+        self.destroy()
 
     def finish(self):
         if not self.bot._quit:
