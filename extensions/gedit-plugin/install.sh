284085b3dfe3f75ed06e5f4ade0fa63f6975b611
Sebastian Oliva <tian2992@gmail.com>
2010-10-03 10:54:24 -0600

    made functional again gedit-plugin, cleaned it up a bit, and added myself to authors

--- extensions/gedit-plugin/install.sh
+++ extensions/gedit-plugin/install.sh
@@ -1,23 +1,29 @@
-#!/bin/sh
+#!/bin/bash
+
+function pluginInstall {
+  cp -r -f shoebotit* ~/.gnome2/gedit/plugins
+  echo 'Gedit plugin installed!'
+}
+
+function installLangSpecs {
+  cp -f shoebot.lang ~/.gnome2/gtksourceview-1.0/language-specs
+  echo 'GTKSourceView language specs installed!'
+}
 
 if [ -d ~/.gnome2/gedit/plugins ]; then
-  cp -f shoebotit* ~/.gnome2/gedit/plugins
-  echo 'Gedit plugin successfully installed!'
+  pluginInstall
 else
   echo 'Local Gedit plugin directory does not exist. Creating it.'
   mkdir -p ~/.gnome2/gedit/plugins
-  cp -f shoebotit* ~/.gnome2/gedit/plugins
-  echo 'Gedit plugin successfully installed!'
+  pluginInstall
 fi
 
 if [ -d ~/.gnome2/gtksourceview-1.0/language-specs ]; then
-  cp -f shoebot.lang ~/.gnome2/gtksourceview-1.0/language-specs
-  echo 'GTKSourceView language specs successfully installed!'
+  installLangSpecs
 else
   echo 'Local GTKSourceView language-specs directory does not exist. Creating it.'
   mkdir -p ~/.gnome2/gtksourceview-1.0/language-specs
-  cp -f shoebot.lang ~/.gnome2/gtksourceview-1.0/language-specs
-  echo 'GTKSourceView language specs successfully installed!'
+  installLangSpecs
 fi
 
-
+echo "All done, enable the shoebotit plugin on gedit's plugin menu"
