f26bd714e5458d982a2c94bc7ef96884cd3a350e
Stuart Axon <stuaxo2@yahoo.com>
2013-01-15 21:52:11 +0000

    Fix ref to gnome2.

--- extensions/gedit3-plugin/install.sh
+++ extensions/gedit3-plugin/install.sh
@@ -7,7 +7,7 @@ function pluginInstall {
 }
 
 function installLangSpecs {
-  cp -f shoebot.lang ~/.gnome2/gtksourceview-3.0/language-specs
+  cp -f shoebot.lang ~/.local/share/gtksourceview-3.0/language-specs
   echo 'GTKSourceView language specs installed!'
 }
 
