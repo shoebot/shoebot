5a4fd2d6bfc34e3bb4d688afe3afdcd45fc4b033
Stuart Axon <stuaxo2@yahoo.com>
2014-01-11 15:23:04 +0000

    Change setup_virtualenv to use the requirements file, filtering the files that work.
    Remove inline comments from requirements since they break setup_virtualenv.sh

--- setup/setup_virtualenv.sh
+++ setup/setup_virtualenv.sh
@@ -1,6 +1,13 @@
+
+
 #!/bin/bash
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+REQUIREMENTS_TXT="$SCRIPTPATH/../requirements/requirements.txt"
+echo $REQUIREMENTS_TXT
+#cat $REQUIREMENTS_TXT
+echo ""
+sed -e '/^#/d' $REQUIREMENTS_TXT | xargs echo
 
 if [ "" = "$VIRTUAL_ENV" ]; then
     echo Activate virtualenv first.
@@ -8,8 +15,10 @@ else
     # TODO - Only tested on Ubuntu, probably need a better way of finding the system python.
     echo Link Gtk from system python...
     ln -sf /usr/lib/python2.7/dist-packages/{glib,gobject,cairo,gtk-2.0,pygtk.py,pygtk.pth} $VIRTUAL_ENV/lib/python2.7/site-packages
+    
     echo Install requirements...
-    # Nanually specify packages issues with pycairo, pygtk are fixed.
-    pip install Pillow==1.7.5 pymetar numpy
+    # Install packages except pygtk, rsvg, pycairo which need to be manually linked.
+    sed -e '/^#/d' -e '/gtk/d' -e '/rsvg/d' -e '/pycairo/d' $REQUIREMENTS_TXT | echo
+    sed -e '/^#/d' -e '/gtk/d' -e '/rsvg/d' -e '/pycairo/d' $REQUIREMENTS_TXT | xargs pip install
 fi
 
