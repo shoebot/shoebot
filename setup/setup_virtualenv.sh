e913f779ce08f112eefbea07a9efbc6f4255df5f
Stuart Axon <stuaxo2@yahoo.com>
2014-01-11 15:31:43 +0000

    Remove whitespace.

--- setup/setup_virtualenv.sh
+++ setup/setup_virtualenv.sh
@@ -10,10 +10,13 @@ if [ "" = "$VIRTUAL_ENV" ]; then
 else
     # TODO - Only tested on Ubuntu, probably need a better way of finding the system python.
     echo Link Glib, Gtk from system python etc
-    ln -sf /usr/lib/python2.7/dist-packages/{glib,gobject,cairo,gtk-2.0,pygtk.py,pygtk.pth} $VIRTUAL_ENV/lib/python2.7/site-packages    
+    ln -sf /usr/lib/python2.7/dist-packages/{glib,gobject,cairo,gtk-2.0,pygtk.py,pygtk.pth} $VIRTUAL_ENV/lib/python2.7/site-packages
     echo Install requirements...
     # Install packages except pygtk, rsvg, pycairo which need to be manually linked.
     sed -e '/^#/d' -e '/gtk/d' -e '/rsvg/d' -e '/pycairo/d' $REQUIREMENTS_TXT | echo
     sed -e '/^#/d' -e '/gtk/d' -e '/rsvg/d' -e '/pycairo/d' $REQUIREMENTS_TXT | xargs pip install
+    pushd $SCRIPTPATH/..
+    python setup.py install
+    popd
 fi
 
