617215c3bb9bc2e4e24629746da3bbb0063f8080
Stuart Axon <stuaxo2@yahoo.com>
2014-01-16 12:33:03 +0000

    Enable setup virtualenv to point at existing virtualenv.

--- setup/setup_virtualenv.sh
+++ setup/setup_virtualenv.sh
@@ -1,13 +1,33 @@
-
-
 #!/bin/bash
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 REQUIREMENTS_TXT="$SCRIPTPATH/../requirements/requirements.txt"
 
-if [ "" = "$VIRTUAL_ENV" ]; then
-    echo Activate virtualenv first.
-else
+
+show_usage() {
+echo "Usage:
+
+setup_virtualenv.sh [-c]|[env-dir|venvwrapper-env-name]
+
+Links Gtk and dependencies from system python and install Shoebot.
+
+
+Install to current virtualenv:
+$ setup_virtualenv.sh -c
+
+Install to virtualenv by path:
+$ setup_virtualenv.sh /path/to/venv
+
+VirtualenvWrapper Example:
+$ mkvirtualenv shoebot
+$ setup_virtualenv.sh shoebot
+"
+}
+
+setup_venv() {
+    VIRTUAL_ENV=$1
+    echo "Setup virtualenv shoebot and link dependencies at ${VIRTUAL_ENV}"
+
     # TODO - Only tested on Ubuntu, probably need a better way of finding the system python.
     echo Link Glib, Gtk from system python etc
     ln -sf /usr/lib/python2.7/dist-packages/{glib,gobject,cairo,gtk-2.0,pygtk.py,pygtk.pth} $VIRTUAL_ENV/lib/python2.7/site-packages
@@ -18,5 +38,28 @@ else
     pushd $SCRIPTPATH/..
     python setup.py install
     popd
+}
+
+
+
+if [ $# -ne 1 ]; then
+    show_usage
+    exit
+else
+    if [ "-c" = "$1" ]; then
+        setup_current_venv
+        if [ "" = "$VIRTUAL_ENV" ]; then
+            echo "Activate virtualenv before specifying -c"
+            exit 1
+        else
+            setup_venv "${VIRTUAL_ENV}"
+        fi
+    elif [ -f "${1}/bin/activate" ]; then
+        setup_venv "${1}"
+    elif [ -f "${HOME}/.virtualenvs/${1}/bin/activate" ]; then
+        setup_venv "${HOME}/.virtualenvs/${1}"
+    else
+        echo "No virtualenv at ${1} or ~/.virtualenvs/${1}"
+    fi
 fi
 
