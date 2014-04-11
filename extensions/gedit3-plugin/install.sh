486631d56a21f85e6841e5d4aa948b3e264b0111
Stuart Axon <stuaxo2@yahoo.com>
2014-04-11 20:27:18 +0100

    Install globally if run as root.

--- extensions/gedit3-plugin/install.sh
+++ extensions/gedit3-plugin/install.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
+if [ "$USER" = "root" ]; then
+  SHARE_DIR="/usr/share"
+  echo Install locally to $SHARE_DIR
+else
+  SHARE_DIR="$HOME/.local/share"
+  echo Install globally to $SHARE_DIR
+fi
+
 function pluginInstall {
-  cp -r -f shoebotit* ~/.local/share/gedit/plugins
+  cp -r -f shoebotit* $SHARE_DIR/gedit/plugins
   echo 'Gedit plugin installed!'
 }
 
 function installLangSpecs {
-  cp -f shoebot.lang ~/.local/share/gtksourceview-3.0/language-specs
-  update-mime-database ~/.local/share/mime
+  cp -f shoebot.lang $SHARE_DIR/gtksourceview-3.0/language-specs
+  update-mime-database $SHARE_DIR/mime
   echo 'GTKSourceView language specs installed!'
 }
 
-if [ -d ~/.gnome2/gedit/plugins ]; then
+if [ -d $SHARE_DIR/gedit/plugins ]; then
   pluginInstall
 else
   echo 'Local Gedit plugin directory does not exist. Creating it.'
-  mkdir -p ~/.local/share/gedit/plugins
+  mkdir -p $SHARE_DIR/gedit/plugins
   pluginInstall
 fi
 
-if [ -d ~/.local/share/gtksourceview-3.0/language-specs ]; then
+if [ -d $SHARE_DIR/gtksourceview-3.0/language-specs ]; then
   installLangSpecs
 else
   echo 'Local GTKSourceView language-specs directory does not exist. Creating it.'
-  mkdir -p ~/.local/share/gtksourceview-3.0/language-specs
+  mkdir -p $SHARE_DIR/gtksourceview-3.0/language-specs
   installLangSpecs
 fi
 
