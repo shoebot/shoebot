0f3b5f1755712cd23f8257bdbf808a78cfa3771f
Stuart Axon <stu@localhost.localdomain>
2014-05-18 17:39:50 +0100

    setup virtualenv now works on debian.

--- install/setup_virtualenv.sh
+++ install/setup_virtualenv.sh
@@ -24,13 +24,44 @@ $ setup_virtualenv.sh shoebot
 "
 }
 
+link_module() {
+    # Bit hacky having this as well as link_sitepackage, but it works...
+    ln -sf `python -c "import os, $1; print os.path.dirname($1.__file__)"` $VIRTUAL_ENV/lib/python$PYTHON_VERSION/site-packages
+}
+
+link_sitepackage() {
+    TARGET=`find ${SITE_PACKAGES//:/ } -maxdepth 1 -name $1 -print -quit`
+    echo $TARGET
+    ln -sf $TARGET $VIRTUAL_ENV/lib/python$PYTHON_VERSION/site-packages
+}
+
 setup_venv() {
     VIRTUAL_ENV=$1
+    PYTHON_VERSION=`python -c "import sys;print sys.version[0:3]"`
+
     echo "Setup virtualenv shoebot and link dependencies at ${VIRTUAL_ENV}"
 
-    # TODO - Only tested on Ubuntu, probably need a better way of finding the system python.
+    
+    echo "Linking to system modules."
+
+    # TODO - Tested on Ubuntu, probably need a better way of finding the system python.
     echo Link Glib, Gtk from system python etc
-    ln -sf /usr/lib/python2.7/dist-packages/{glib,gobject,cairo,gtk-2.0,pygtk.py,pygtk.pth} $VIRTUAL_ENV/lib/python2.7/site-packages
+
+    _PATH=$PATH
+    export PATH=${PATH#*:}
+
+    SITE_PACKAGES=`python -c "import site; print ':'.join(site.getsitepackages())"`
+
+    #ln -sf $SYS_MODULES/{glib,gobject,cairo,gtk-2.0,pygtk.py,pygtk.pth} $VIRTUAL_ENV/lib/python2.7/site-packages
+    link_module cairo
+    link_sitepackage glib
+    link_sitepackage gobject
+    link_sitepackage pygtk.py
+    link_sitepackage pygtk.pth
+    link_sitepackage gtk-2.0
+
+    export PATH=$_PATH
+
     echo Install requirements...
     # Install packages except pygtk, rsvg, pycairo which need to be manually linked.
     sed -e '/^#/d' -e '/gtk/d' -e '/rsvg/d' -e '/pycairo/d' $REQUIREMENTS_TXT | echo
@@ -47,11 +78,12 @@ if [ $# -ne 1 ]; then
     exit
 else
     if [ "-c" = "$1" ]; then
-        setup_current_venv
         if [ "" = "$VIRTUAL_ENV" ]; then
             echo "Activate virtualenv before specifying -c"
             exit 1
         else
+            # Remove virtualenv from the path.
+            echo $PATH
             setup_venv "${VIRTUAL_ENV}"
         fi
     elif [ -f "${1}/bin/activate" ]; then
