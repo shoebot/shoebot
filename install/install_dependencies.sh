3e1afac221d07ec3819699e2e3ef9cef83ce1d19
Stuart Axon <stuaxo2@yahoo.com>
2014-05-19 22:21:33 +0100

    Add the other OSs to the dependency installer.

--- install/install_dependencies.sh
+++ install/install_dependencies.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+REDHAT_PACKAGES="libjpeg-devel pycairo pygtk2 pygobject2 gnome-python2-rsvg python-imaging"
+SUSE_PACKAGES="libjpeg-devel python-pycairo python-gtk python-pygobject2 python-rsvg python-imaging"
+
+DEBIAN_PACKAGES="python2.7-dev libjpeg-dev python-cairo python-gtk2 python-gobject python-gtksourceview2 python-rsvg"
+UBUNTU_PACKAGES="libjpeg-dev python-cairo python-gtk2 python-gobject python-gtksourceview2 python-rsvg"
+
+
 install_debian() {
-    sudo apt-get install python2.7-dev libjpeg-dev python-cairo python-gtk2 python-gobject python-gtksourceview2 python-rsvg
+    sudo apt-get install ${DEBIAN_PACKAGES}
 }
 
 install_ubuntu() {
-    sudo apt-get install libjpeg-dev python-cairo python-gtk2 python-gobject python-gtksourceview2 python-rsvg
+    sudo apt-get install ${UBUNTU_PACKAGES}
 }
 
+install_redhat() {
+    sudo yum install ${REDHAT_PACKAGES}
+}
 
+install_suse() {
+    sudo zypper install ${SUSE_PACKAGES}
+}
 
 ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
 
@@ -20,8 +33,10 @@ elif [ -f /etc/debian_version ]; then
     OS=Debian  # XXX or Ubuntu??
     VER=$(cat /etc/debian_version)
 elif [ -f /etc/redhat-release ]; then
-    # TODO add code for Red Hat and CentOS here
-    echo 'TODO - Add redhat/centos support'
+    OS=Redhat
+elif [ -f /etc/SuSE-release ]; then
+    OS=SuSE
+    VER=$(grep VERSION /etc/SuSE-release)
 else
     OS=$(uname -s)
     VER=$(uname -r)
@@ -33,6 +48,10 @@ if [ "Debian" = "$OS" ]; then
     install_debian
 elif [ "Ubuntu" = "$OS" ]; then
     install_ubuntu
+elif [ "Redgat" = "$OS" ]; then
+    install_redhat
+elif [ "SuSE" = "$OS" ]; then
+    install_suse
 else
     echo TODO Add code for $OS
 fi
