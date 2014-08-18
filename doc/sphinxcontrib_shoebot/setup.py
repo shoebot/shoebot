cb955444b12acfbbbea5835d95e2804a54335198
Stuart Axon <stuaxo2@yahoo.com>
2014-08-18 16:49:31 +0100

    Try using namespace_packages with actual package name.

--- doc/sphinxcontrib_shoebot/setup.py
+++ doc/sphinxcontrib_shoebot/setup.py
@@ -43,5 +43,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=requires,
-    namespace_packages=['sphinxcontrib'],
+    namespace_packages=['sphinxcontrib_shoebot'],
 )
