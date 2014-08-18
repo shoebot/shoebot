71b867d79cd04fcd6094cf208bc378e69e4f5743
Stuart Axon <stuaxo2@yahoo.com>
2014-08-18 23:50:55 +0100

    Add comment.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,6 +25,10 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
+
+
 # If your extensions (or modules documented by autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
