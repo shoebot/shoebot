d246cfb121fc9156b2ee3319f0a78e3190e4353c
Stuart Axon <stuaxo2@yahoo.com>
2015-04-16 00:49:15 +0700

    Fix typo

--- extensions/gedit3-plugin/shoebotit/__init__.py
+++ extensions/gedit3-plugin/shoebotit/__init__.py
@@ -154,7 +154,7 @@ class ShoebotWindowHelper(object):
     def disconnect_change_handler(self, doc):
         if self.changed_handler_id is not None:
             doc.disconnect(self.changed_handler_id)
-            self.changed_hander_id = None
+            self.changed_handler_id = None
 
     def get_source(self, doc):
         """
