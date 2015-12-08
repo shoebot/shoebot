d91d449e9646b6105631e5138c0990779dccca79
Stuart Axon <stuaxo2@yahoo.com>
2015-12-08 19:18:13 +0800

    Don't die quite as much if pycairo not available

--- shoebot/data/typography.py
+++ shoebot/data/typography.py
@@ -27,7 +27,10 @@
 #   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 #   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 #   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-import cairo as pycairo
+try:
+    import cairo as pycairo
+except ImportError:
+    pycairo = None
 import cairocffi as cairo
 
 try:
