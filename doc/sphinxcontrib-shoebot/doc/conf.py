148b2e1ccf7a043cc5046ef992c0c94796791fa4
Stuart Axon <stuaxo2@yahoo.com>
2016-04-26 23:23:56 +0100

    Undo that change :/

--- doc/sphinxcontrib-shoebot/doc/conf.py
+++ doc/sphinxcontrib-shoebot/doc/conf.py
@@ -84,7 +84,7 @@ exclude_trees = ['_build']
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-#pygments_style = 'python'
+pygments_style = 'python'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
