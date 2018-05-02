d2225c3890fc75a0df0ce562ae8641b50f61dd6b
Stuart Axon <stu.axon@gmail.com>
2018-05-02 01:42:57 +0100

    Fixup gedit 3.12 plugin paths.

--- extensions/gedit/gedit3.12-plugin/install.py
+++ extensions/gedit/gedit3.12-plugin/install.py
@@ -4,17 +4,16 @@
 
 from __future__ import print_function
 
-from os.path import abspath, dirname, exists, expanduser, expandvars, isdir, islink, lexists, join, normpath
-from glob import glob
-
 import errno
 import os
 import shutil
 import stat
 import sys
+from os.path import abspath, dirname, exists, expanduser, expandvars, isdir, islink, lexists, join, normpath
 
 here = dirname(abspath(__file__))
-source_dirs = [here, normpath(join(here, '../../lib'))]
+source_dirs = [here, normpath(join(here, '../../../shoebot/extension'))]
+
 
 def has_admin():
     if os.name == 'nt':
@@ -31,10 +30,12 @@ def has_admin():
         else:
             return (os.environ['USER'], False)
 
+
 def copytree(src, dst, symlinks=False, ignore=None):
     """
     copytree that works even if folder already exists
     """
+    print('src: ', src, 'dest: ', dst)
     # http://stackoverflow.com/questions/1868714/how-do-i-copy-an-entire-directory-of-files-into-an-existing-directory-using-pyth
     if not exists(dst):
         os.makedirs(dst)
@@ -61,14 +62,16 @@ def copytree(src, dst, symlinks=False, ignore=None):
         else:
             shutil.copy2(s, d)
 
+
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc: # Python >2.5
+    except OSError as exc:  # Python >2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else:
             raise
+
 
 def get_dirs_nt(is_admin):
     dirs = {}
@@ -78,42 +81,45 @@ def get_dirs_nt(is_admin):
         dirs = dict(
             dest_dir=dest_dir,
             language_dir=expandvars("%ProgramFiles%\\gedit\\share\\gtksourceview-3.0\\language-specs"),
-            plugin_dir=expandvars("%ProgramFiles%\\gedit\\lib\\gedit-3\\plugins"),                
+            plugin_dir=expandvars("%ProgramFiles%\\gedit\\lib\\gedit-3\\plugins"),
         )
     else:
         dest_dir = expandvars("%UserProfile%//AppData//Roaming"),
         dirs = dict(
             dest_dir=dest_dir,
-            language_dir=None,  # TODO
+            language_dir=None,  # Not implemented yet
             plugin_dir=join(dest_dir, "gedit//plugins"),
         )
     return dirs
 
+
 def get_dirs_unix(is_admin):
     if is_admin:
         if isdir('/usr/lib64'):
-            dest_dir="/usr/lib64"
+            dest_dir = "/usr/lib64"
         else:
-            dest_dir="/usr/lib"
+            dest_dir = "/usr/lib"
     else:
-        dest_dir=expanduser("~/.local/share")
+        dest_dir = expanduser("~/.local/share")
 
-    dirs=dict(
+    dirs = dict(
         dest_dir=dest_dir,
         language_dir=join(dest_dir, "gtksourceview-3.0/language-specs"),
         plugin_dir=join(dest_dir, "gedit/plugins"),
     )
-    
+
     return dirs
+
 
 if os.name == 'nt':
     get_dirs = get_dirs_nt
 else:
     get_dirs = get_dirs_unix
 
+
 def install_plugin(name=None, dest_dir=None, plugin_dir=None, language_dir=None, is_admin=False):
     if is_admin and not isdir(plugin_dir):
-        print('%s not found' % name)
+        sys.stderr.write('%s not found' % name)
         sys.exit(1)
     else:
         if not is_admin:
@@ -129,11 +135,12 @@ def install_plugin(name=None, dest_dir=None, plugin_dir=None, language_dir=None,
     print('install %s plugin to %s' % (name, dest_dir))
     source_dir = None
     try:
+        copytree(here, plugin_dir)
         for source_dir in source_dirs:
             copytree(source_dir, plugin_dir)
     except Exception as e:
-        print('error attempting to copy %s' % source_dir)
-        print(e)
+        sys.stderr.write('error attempting to copy %s' % source_dir)
+        sys.stderr.write(str(e))
         sys.exit(1)
 
     if language_dir:
@@ -141,21 +148,21 @@ def install_plugin(name=None, dest_dir=None, plugin_dir=None, language_dir=None,
         shutil.copyfile(join(here, "shoebot.lang"), join(language_dir, "shoebot.lang"))
         os.system("update-mime-database %s/mime" % dest_dir)
 
-    os.system("glib-compile-schemas %s/gedit/plugins/shoebotit" % dest_dir) ## FIXME, kind of specific to gedit...
-    print('success')
+    os.system("glib-compile-schemas %s/gedit/plugins/shoebotit" % dest_dir)
 
 
 def main():
     username, is_admin = has_admin()
-    
+
     dirs = get_dirs(is_admin)
     kwargs = dict(
-        name = "gedit-3.12",
-        is_admin = is_admin,
+        name="gedit-3.12",
+        is_admin=is_admin,
         **dirs
     )
 
     install_plugin(**kwargs)
+
 
 if __name__ == '__main__':
     main()
