f25d744d1c7b5fb5fe96f841d77cecda3ebd5440
Ricardo Lafuente <r@manufacturaindependente.org>
2018-05-11 11:30:59 +0100

    Style edit library code

--- lib/sbaudio/__init__.py
+++ lib/sbaudio/__init__.py
@@ -1,13 +1,7 @@
-from pysoundcard import InputStream, continue_flag
-
 import atexit
-import collections
-import time
 import threading
-import math
 import numpy as np
-import struct
-
+from pysoundcard import InputStream
 from pysoundcard import device_info
 from fuzzywuzzy import fuzz
 
@@ -16,38 +10,40 @@ BUFF_LENGTH = 1024
 CHANNELS = 2
 NUM_SAMPLES = 512
 
+
 def fft_bandpassfilter(data, fs, lowcut, highcut):
     """
     http://www.swharden.com/blog/2009-01-21-signal-filtering-with-python/#comment-16801
     """
     fft = np.fft.fft(data)
-    n = len(data)
-    timestep = 1.0 / fs
-    freq = np.fft.fftfreq(n, d = timestep)
+    # n = len(data)
+    # timestep = 1.0 / fs
+    # freq = np.fft.fftfreq(n, d=timestep)
     bp = fft.copy()
 
     # Zero out fft coefficients
-    #bp[10:-10] = 0
+    # bp[10:-10] = 0
 
     # Normalise
-    #bp *= real(fft.dot(fft))/real(bp.dot(bp))
+    # bp *= real(fft.dot(fft))/real(bp.dot(bp))
 
     bp *= fft.dot(fft) / bp.dot(bp)
-    
+
     # must multipy by 2 to get the correct amplitude
     ibp = 12 * np.fft.ifft(bp)
     return ibp
 
-    #for i in range(len(bp)):
-    #    if freq[i] >= highcut or freq[i] < lowcut:
-    #        bp[i] = 0
+    # for i in range(len(bp)):
+    #     if freq[i] >= highcut or freq[i] < lowcut:
+    #         bp[i] = 0
 
-    #ibp = np.fft.ifft(bp)
-    #ibp = 2.0 * np.fft.ifft(bp)
-    #print ibp
-    #return ibp
+    # ibp = np.fft.ifft(bp)
+    # ibp = 2.0 * np.fft.ifft(bp)
+    # print ibp
+    # return ibp
 
-def flatten_fft(scale = 1.0):
+
+def flatten_fft(scale=1.0):
     """
     Produces a nicer graph, I'm not sure if this is correct
     """
@@ -55,25 +51,28 @@ def flatten_fft(scale = 1.0):
     for i, v in enumerate(audio.spectrogram):
         yield scale * (i * v) / _len
 
-def scaled_fft(fft, scale = 1.0):
+
+def scaled_fft(fft, scale=1.0):
     """
     Produces a nicer graph, I'm not sure if this is correct
     """
     data = np.zeros(len(fft))
     for i, v in enumerate(fft):
-         data[i] = scale * (i * v) / NUM_SAMPLES
-         
+        data[i] = scale * (i * v) / NUM_SAMPLES
+
     return data
 
+
 def triple(spectrogram):
-    #c = spectrogram.copy()
-    #c.resize(3, 255 / 3)
-    #return c
+    # c = spectrogram.copy()
+    # c.resize(3, 255 / 3)
+    # return c
     bass = spectrogram[0:85]
     mid = spectrogram[85:-85]
     treble = spectrogram[-85:-1]
 
     return bass, mid, treble
+
 
 def fuzzydevices(match='', min_ratio=30):
     device_ratios = []
@@ -85,9 +84,11 @@ def fuzzydevices(match='', min_ratio=30):
     for ratio, device in sorted(device_ratios, key=lambda ratio_device: (ratio_device[0])):
         yield device
 
+
 def firstfuzzydevice(match=''):
     devices = list(fuzzydevices(match, 0))
     return devices[0]
+
 
 class AudioException(Exception):
     def __init__(self, msg):
@@ -97,17 +98,18 @@ class AudioException(Exception):
     def __str__(self):
         return self.msg
 
+
 class AudioThread(threading.Thread):
     def __init__(self):
         # TODO - This could be a bit less dumb, and only run
         #        bandpass (or other) filters if they are actually used
         super(self.__class__, self).__init__()
-                
+
         self.daemon = True
 
         self.streams = {}
         self.running = False
-        
+
         self._spectrogram = np.zeros(NUM_SAMPLES)
         self._bandpassed = np.zeros(NUM_SAMPLES)
 
@@ -119,14 +121,10 @@ class AudioThread(threading.Thread):
         with InputStream(samplerate=44100, blocksize=16) as s:
             while self.running:
                 vec = s.read(NUM_SAMPLES)
-
                 # Downsample to mono
                 mono_vec = vec.sum(-1) / float(s.channels)
-
                 self._spectrogram = np.fft.fft(mono_vec)
-
                 self._bandpassed = fft_bandpassfilter(self.spectrogram, 44010, 100, 20)
-
 
     def autostart(self):
         if not self.running:
@@ -137,7 +135,7 @@ class AudioThread(threading.Thread):
     def quit(self):
         """
         Shutdown the audio thread
-        """        
+        """
         if self.running:
             self.running = False
             self.join()
@@ -152,5 +150,5 @@ class AudioThread(threading.Thread):
         self.autostart()
         return self._bandpassed
 
-audio = AudioThread()
 
+audio = AudioThread()
--- lib/sbvideo/__init__.py
+++ lib/sbvideo/__init__.py
@@ -1,23 +1,24 @@
 # Shoebot Video Library 0.2
 # Copyright: HVA - Hermanitos Verdes Architetti, 2009
 # licenSe: LGPL
-
-__author__    = "Francesco Fantoni"
-__version__   = "0.2"
-__copyright__ = "Copyright (c) 2009, HVA - Hermanitos Verdes Architetti"
-__license__   = "lgpl"
-
-
-import cairo, numpy
+import cairo
+import numpy
 import opencv
 from opencv import highgui as hg
 import os
 
+__author__ = "Francesco Fantoni"
+__version__ = "0.2"
+__copyright__ = "Copyright (c) 2009, HVA - Hermanitos Verdes Architetti"
+__license__ = "lgpl"
+
+
 class SBVideoError(RuntimeError):
     pass
 
+
 class Movie:
-    
+
     def __init__(self, path, start=0, stop=None):
         self.path = path
         self.video = hg.cvCreateFileCapture(self.path)
@@ -25,17 +26,17 @@ class Movie:
             raise SBVideoError("Could not open stream %s" % self.path)
 
         # these functions don't seem to work at present on my linux system
-        
-        #self.fps = hg.cvGetCaptureProperty(self.video, hg.CV_CAP_PROP_FPS)
-        #self.n_of_frames = hg.cvGetCaptureProperty(self.video, hg.CV_CAP_PROP_FRAME_COUNT)
-        #self.duration = self.n_of_frames/self.fps
-        #self.width = hg.cvGetCaptureProperty(self.video, hg.CV_CAP_PROP_FRAME_WIDTH)
-        #self.height = hg.cvGetCaptureProperty(self.video, hg.CV_CAP_PROP_FRAME_HEIGHT)
+
+        # self.fps = hg.cvGetCaptureProperty(self.video, hg.CV_CAP_PROP_FPS)
+        # self.n_of_frames = hg.cvGetCaptureProperty(self.video, hg.CV_CAP_PROP_FRAME_COUNT)
+        # self.duration = self.n_of_frames/self.fps
+        # self.width = hg.cvGetCaptureProperty(self.video, hg.CV_CAP_PROP_FRAME_WIDTH)
+        # self.height = hg.cvGetCaptureProperty(self.video, hg.CV_CAP_PROP_FRAME_HEIGHT)
         hg.cvSetCaptureProperty(self.video, hg.CV_CAP_PROP_POS_FRAMES, start)
-    
+
     def frame(self, t=None):
         frame = MovieFrame(src=self.video, time=t)
-        return frame                
+        return frame
 
 
 def movie(path, start=0, stop=None):
@@ -43,7 +44,7 @@ def movie(path, start=0, stop=None):
 
 
 class Camera:
-    
+
     def __init__(self, cam=0, width=None, height=None):
         self.path = cam
         self.video = hg.cvCreateCameraCapture(self.path)
@@ -51,10 +52,11 @@ class Camera:
             hg.cvSetCaptureProperty(self.video, hg.CV_CAP_PROP_FRAME_WIDTH, width)
         if height:
             hg.cvSetCaptureProperty(self.video, hg.CV_CAP_PROP_FRAME_HEIGHT, height)
-    
+
     def frame(self, t=None):
         frame = MovieFrame(src=self.video, time=None)
         return frame
+
 
 def camera(cam=0, width=None, height=None):
     return Camera(cam, width, height)
@@ -67,48 +69,46 @@ class MovieFrame:
         self.src = src
         self.time = time
         if self.time:
-            hg.cvSetCaptureProperty(self.src, hg.CV_CAP_PROP_POS_FRAMES, self.time)    
+            hg.cvSetCaptureProperty(self.src, hg.CV_CAP_PROP_POS_FRAMES, self.time)
         self.iplimage = hg.cvQueryFrame(self.src)
         self.width = self.iplimage.width
         self.height = self.iplimage.height
-        self.image = opencv.cvCreateImage(opencv.cvGetSize(self.iplimage),8, 4)
-        opencv.cvCvtColor(self.iplimage,self.image,opencv.CV_BGR2BGRA)
+        self.image = opencv.cvCreateImage(opencv.cvGetSize(self.iplimage), 8, 4)
+        opencv.cvCvtColor(self.iplimage, self.image, opencv.CV_BGR2BGRA)
         self.buffer = numpy.fromstring(self.image.imageData, dtype=numpy.uint32).astype(numpy.uint32)
         self.buffer.shape = (self.image.width, self.image.height)
         self.time = hg.cvGetCaptureProperty(self.src, hg.CV_CAP_PROP_POS_MSEC)
-                
+
     def _data(self):
-        return cairo.ImageSurface.create_for_data(self.buffer, cairo.FORMAT_RGB24, self.width, self.height, self.width*4)
-        
+        return cairo.ImageSurface.create_for_data(self.buffer, cairo.FORMAT_RGB24, self.width, self.height, self.width * 4)
+
     data = property(_data)
-    
+
     def detectObject(self, classifier):
         self.grayscale = opencv.cvCreateImage(opencv.cvGetSize(self.iplimage), 8, 1)
         opencv.cvCvtColor(self.iplimage, self.grayscale, opencv.CV_BGR2GRAY)
         self.storage = opencv.cvCreateMemStorage(0)
         opencv.cvClearMemStorage(self.storage)
         opencv.cvEqualizeHist(self.grayscale, self.grayscale)
-        
+
         try:
-            self.cascade = opencv.cvLoadHaarClassifierCascade(os.path.join(os.path.dirname(__file__), classifier+".xml"),opencv.cvSize(1,1))
+            self.cascade = opencv.cvLoadHaarClassifierCascade(os.path.join(os.path.dirname(__file__), classifier+".xml"),opencv.cvSize(1, 1))
         except:
-            raise AttributeError, "could not load classifier file"            
-        
-        self.objects = opencv.cvHaarDetectObjects(self.grayscale, self.cascade, self.storage, 1.2, 2, opencv.CV_HAAR_DO_CANNY_PRUNING, opencv.cvSize(50,50))
-        
-        return self.objects        
-        
+            raise AttributeError, "could not load classifier file"
+
+        self.objects = opencv.cvHaarDetectObjects(self.grayscale, self.cascade, self.storage, 1.2, 2, opencv.CV_HAAR_DO_CANNY_PRUNING, opencv.cvSize(50, 50))
+
+        return self.objects
+
     def _faces(self):
         classifier = "haarcascade_frontalface_alt"
         return self.detectObject(classifier)
     faces = property(_faces)
-    
+
+
 class objecttest:
-    
     def __init__(self, i):
-        self.x=1*i
-        self.y=1*i
-        self.width=10*i
-        self.height=10*i 
-        
-                              
+        self.x = 1 * i
+        self.y = 1 * i
+        self.width = 10 * i
+        self.height = 10 * i
