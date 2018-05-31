edbe0a9e78ff7cbccd23efbeca7c6c92630dafb8
Ricardo Lafuente <r@manufacturaindependente.org>
2018-05-31 19:15:21 +0100

    Revert "snapshot() now works with surfaces as a target"
    This reverts commit 388f411bcb248f21bee27e260a2d467a5fc74da5.

--- shoebot/grammar/bot.py
+++ shoebot/grammar/bot.py
@@ -320,10 +320,11 @@ class Bot(Grammar):
         # Taken ipsis verbis from Nodebox
         return glob(path)
 
-    def snapshot(self, target=None, defer=None, autonumber=False):
+    def snapshot(self, filename=None, surface=None, defer=None, autonumber=False):
         '''Save the contents of current surface into a file or cairo surface/context
 
-        :param filename: Can be a filename or a Cairo surface.
+        :param filename: Filename to save snapshot as, available formats include .png, .ps, .svg
+        :param surface:  If specified will output snapshot to the supplied cairo surface.
         :param defer: If true, buffering/threading may be employed however output will not be immediate.
         :param autonumber: If true then a number will be appended to the filename.
         '''
@@ -331,30 +332,22 @@ class Bot(Grammar):
             file_number = self._frame
         else:
             file_number = None
-
-        import cairocffi as cairo
-        if isinstance(target, cairo.Surface):
-            # snapshot to Cairo surface
+        if surface:
             if defer is None:
                 defer = False
-            ctx = cairo.Context(target)
-            # this used to be self._canvas.snapshot, but I couldn't make it work.
-            # self._canvas.snapshot(target, defer)
-            # TODO: check if this breaks when taking more than 1 snapshot
-            self._canvas._drawqueue.render(ctx)
+            self._canvas.snapshot(surface, defer)
             return
-        elif target is None:
-            # If nothing specified, use a default filename from the script name
+        elif filename is None:
+            # If nothing specied, we can see if a filename is available
             script_file = self._namespace.get('__file__')
             if script_file:
-                target = os.path.splitext(script_file)[0] + '.svg'
+                filename = os.path.splitext(script_file)[0] + '.svg'
                 file_number = True
 
-        if target:
-            # snapshot to file, target is a filename
+        if filename:
             if defer is None:
                 defer = True
-            self._canvas.snapshot(target, defer=defer, file_number=file_number)
+            self._canvas.snapshot(filename, defer=defer, file_number=file_number)
         else:
             raise ShoebotError('No image saved')
 
