5dcb3113cf384c2464e9297424aa93bb92ce8a32
Ricardo Lafuente <r@manufacturaindependente.org>
2019-06-01 17:54:57 +0100

    Remove example dependencies and add import warnings

--- setup.py
+++ setup.py
@@ -128,22 +128,12 @@ BASE_REQUIREMENTS = [
     "pubsub==0.1.1",
 ]
 
-# Requirements to run examples
-EXAMPLE_REQUIREMENTS = [
-    "fuzzywuzzy==0.5.0",  # sbaudio
-    "planar",  # examples
-    "PySoundCard>=0.5.2",  # sbaudio
-]
-if not is_pypy:
-    EXAMPLE_REQUIREMENTS.append("numpy>=1.9.1")
 
-
-def requirements(debug=True, with_examples=True, with_pgi=None):
+def requirements(debug=True, with_pgi=None):
     """
     Build requirements based on flags
 
     :param with_pgi: Use 'pgi' instead of 'gi' - False on CPython, True elsewhere
-    :param with_examples:
     :return:
     """
     reqs = list(BASE_REQUIREMENTS)
@@ -153,16 +143,12 @@ def requirements(debug=True, with_examples=True, with_pgi=None):
     if debug:
         print("setup options: ")
         print("with_pgi:      ", "yes" if with_pgi else "no")
-        print("with_examples: ", "yes" if with_examples else "no")
     if with_pgi:
         reqs.append("pgi")
         if debug:
             print("warning, as of April 2019 typography does not work with pgi")
     else:
         reqs.append(PYGOBJECT)
-
-    if with_examples:
-        reqs.extend(EXAMPLE_REQUIREMENTS)
 
     if debug:
         print("")
@@ -187,7 +173,6 @@ setup(
     setup_requires=[PYCAIRO],
     install_requires=requirements(
         debug="install" in sys.argv,
-        with_examples="SHOEBOT_SKIP_EXAMPLES" not in os.environ,
         with_pgi=os.environ.get("SHOEBOT_GI", False) == "pgi",
     ),
     entry_points={
