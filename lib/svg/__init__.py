58a5c993485380bf31676511e7304cbb310d5d5d
Ricardo Lafuente <r@manufacturaindependente.org>
2019-11-19 22:49:40 +0000

    Remove leftover print statements

--- lib/svg/__init__.py
+++ lib/svg/__init__.py
@@ -258,7 +258,6 @@ def parse_path(e):
             for i in range(int(len(points)/2)):
                 # More than one value is an implicit LINETO
                 # see https://www.w3.org/TR/SVG/paths.html#PathDataMovetoCommands
-                print(i)
                 if i == 0:
                     _ctx.moveto(points[i*2], points[i*2+1])
                 else:
@@ -274,7 +273,6 @@ def parse_path(e):
             for i in range(int(len(points)/2)):
                 # More than one value is an implicit LINETO
                 # see https://www.w3.org/TR/SVG/paths.html#PathDataMovetoCommands
-                print(i)
                 if i == 0:
                     _ctx.moveto(dx+points[i*2], dy+points[i*2+1])
                 else:
