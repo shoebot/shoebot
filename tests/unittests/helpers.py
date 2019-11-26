390e5366670842b4cd62aee9c9b58a06bb946bd9
Stuart Axon <stuaxo2@yahoo.com>
2019-11-26 01:05:33 +0000

    Feature/test example output (#252)
    * Initial functionality to check examples vs expected.
    
    * Update README.md
    
    * Rename tests.
    
    * Diagnose should run before tests.

---
+++ tests/unittests/helpers.py
@@ -0,0 +1,55 @@
+import filecmp
+
+from pathlib import Path
+from random import seed
+from unittest import TestCase
+
+from shoebot import create_bot
+
+
+class ShoebotTestCase(TestCase):
+    output_dir = Path(__file__).parent.absolute() / "output"
+    paths = [".", "../.."]  # When specifying a filename these paths will be searched.
+
+    def assertOutputFilesEqual(self, file1, file2):
+        """
+        Assert that the two passed in filenames are identical.
+        """
+        # TODO it would be fairly straightforward to check the contents of images if this is needed.
+        self.assertTrue(filecmp.cmp(file1, file2), f"Files differ: {file1} {file2}")
+
+    def assertOutputFile(self, filename):
+        """
+        Verify file exists and is more than 0 bytes.
+        """
+        self.assertTrue(Path(filename).is_file(), f"{filename} does not exist")
+        self.assertNotEqual(0, Path(filename).stat().st_size, f"{filename} is zero bytes")
+
+    def run_code(self, code, outputfile):
+        """
+        Run shoebot code, sets random.seed to stabilize output.
+        """
+        bot = create_bot(outputfile=outputfile)
+        seed(0)
+        bot.run(code)
+
+    def run_filename(self, filename, outputfile):
+        """
+        Run shoebot from filename.
+
+        Paths in ShoebotTestCase.paths will be seatched for the file.
+
+        random.seed is set, to stabilize output.
+
+        outputfile is passed to run_code, e.g. "output.png".
+
+        :param outputfile: File with supported shoebot supported extension, e.g. .png.
+        """
+        for path in self.paths:
+            full_path = Path(path) / filename
+            if full_path.is_file():
+                with open(full_path) as f:
+                    self.run_code(f.read(), outputfile)
+                    return
+        else:
+            raise ValueError(f"Could not find bot {filename} in paths {self.paths}")
--- tests/unittests/test_file_outputs.py
+++ tests/unittests/test_file_outputs.py
@@ -5,19 +5,12 @@ and that none are zero bytes long.
 import tempfile
 import unittest
 
-from pathlib import Path
 from parameterized import parameterized
 
-from shoebot import create_bot
+from tests.unittests.helpers import ShoebotTestCase
 
-class TestOutputFormats(unittest.TestCase):
 
-    def assertOutputFile(self, filename):
-        """
-        Verify file exists and is more than 0 bytes.
-        """
-        self.assertTrue(Path(filename).is_file(), f"{filename} does not exist")
-        self.assertNotEqual(0, Path(filename).stat().st_size, f"{filename} is zero bytes")
+class TestOutputFormats(ShoebotTestCase):
 
     @parameterized.expand(["png", "ps", "pdf", "svg"])
     def test_output_formats(self, file_format):
@@ -25,11 +18,11 @@ class TestOutputFormats(unittest.TestCase):
         Run a simple bot for each supported output format and verify the output.
         """
         with tempfile.NamedTemporaryFile(suffix=f".{file_format}") as f:
-            bot = create_bot(outputfile=f.name)
-            
-            bot.run("background(0)")
+            self.run_code("background(0)",
+                          outputfile=f.name)
 
             self.assertOutputFile(f.name)
 
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(buffer=True)
