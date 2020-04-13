0203b3a997bad0f8c3f8589472628563f62aece6
Stu <stu@MINI.fritz.box>
2020-04-14 00:29:01 +0100

    WIP: Adjust install/install_dependencies.sh to work better with travis. Use travis's own support for homebrew and apt.

--- install/install_dependencies.sh
+++ install/install_dependencies.sh
@@ -1,90 +1,82 @@
 #!/bin/bash
 
-set -ex
+#set -ex
 
-# Linux
-REDHAT_PACKAGES="redhat-rpm-config gcc cairo-devel libjpeg-devel python3-devel python3-gobject cairo-gobject"
-# Fedora == Redhat, these are not detected separately.
+# Fedora, Redhat, (Centos?)
+FEDORA_PACKAGES="redhat-rpm-config gcc cairo-devel libjpeg-devel python3-devel python3-gobject cairo-gobject"
 
+# SuSE:
 SUSE_PACKAGES="gcc libjpeg62-devel python-gobject python-gobject-cairo"
 
-DEB_PACKAGES="build-essential libjpeg-dev python3-dev python-gi-cairo python-gobject gir1.2-rsvg-2.0"
-DEBIAN_PACKAGES=${DEB_PACKAGES}
-UBUNTU_PACKAGES=${DEB_PACKAGES}
-MINT_PACKAGES=${DEB_PACKAGES}
+# Debian, Ubuntu, Mint
+DEB_PACKAGES="build-essential gir1.2-rsvg-2.0 gobject-introspection libglib2.0-dev libjpeg-dev python-gi-cairo python-gobject python3-dev"
 
-
-# OSX
+# Mac OSX
 HOMEBREW_PACKAGES="gtk+3 pygobject3 gobject-introspection jpeg librsvg libffi cairo py3cairo gtksourceview3"
 MACPORTS_PACKAGES="gtk3 py37-gobject gobject-introspection jpeg librsvg cairo cairo-devel py37-cairo gtksourceview3"
 
-deb_install() {
-    sudo apt-get install -y $*
-    hash -r
+install_apt() {
+    sudo apt-get install -y $PACKAGES
 }
 
-install_mint() {
-    deb_install $MINT_PACKAGES
+install_yum() {
+    sudo yum install -y $PACKAGES
 }
 
-install_debian() {
-    deb_install $DEBIAN_PACKAGES
+install_zypper() {
+    sudo zypper install $PACKAGES
 }
 
-install_ubuntu() {
-    deb_install $UBUNTU_PACKAGES
+install_homebrew() {
+    brew install $PACKAGES
 }
 
-install_redhat() {
-    sudo yum install -y $REDHAT_PACKAGES
-    hash -r
+install_macports() {
+    >&2 echo "Using macports, this is unsupported, let us know if it works."
+    sudo port install $PACKAGES
 }
 
-install_suse() {
-    sudo zypper install $SUSE_PACKAGES
-    hash -r
-}
-
-install_darwin() {
+get_osx_packages_and_installer() {
     DIR=$(cd $(dirname "$0"); pwd)
 
-    MACPORTS=$(command -v port > /dev/null; echo $?)
-    HOMEBREW=$(command -v brew > /dev/null; echo $?)
-    
-    if [ "${HOMEBREW},${MACPORTS}" = "1,1" ]; then
+    HAS_HOMEBREW=$(! command -v brew > /dev/null; echo $?)
+    HAS_MACPORTS=$(! command -v port > /dev/null; echo $?)
+
+    if [ "${HAS_HOMEBREW},${HAS_MACPORTS}" = "1,1" ]; then
         echo "Install homebrew and re-run this script"
+        exit 1
     fi
-    if [ "${HOMEBREW},${MACPORTS}" = "0,0" ]; then
+    if [ "${HAS_HOMEBREW},${HAS_MACPORTS}" = "0,0" ]; then
         echo "Choose environment to install shoebot and depencencies:"
         echo "1. Homebrew [recommneded]"
         echo "2. Macports [unsupported]"
         echo "3. Exit"
-        read -p ": " macenv
-        if [ "$macenv" = "1" ]; then
-          unset MACPORTS
-        elif [ "$macenv" = "2" ]; then
-          unset HOMEBREW
+        read -p ": " CHOICE
+        if [ "CHOICE" = "1" ]; then
+          HAS_MACPORTS=0
+        elif [ "$CHOICE" = "2" ]; then
+          HAS_HOMEBREW=0
         else
           exit 1
         fi
     fi
-        
 
-    if [ "${HOMEBREW}" = "0" ]; then
-        echo "Installing Shoebot dependencies on MacOSX Homebrew"
-        brew install $HOMEBREW_PACKAGES
+    if [ "${HAS_HOMEBREW},${HAS_MACPORTS}" = "1,0" ]; then
+      export PACKAGE_MANAGER=brew
+      export PACKAGES=$HOMEBREW_PACKAGES
+      export INSTALL=install_homebrew
+    elif [ "${HAS_HOMEBREW},${HAS_MACPORTS}" = "0,1" ]; then
+      export PACKAGE_MANAGER=port
+      export PACKAGES=$MACPORTS_PACKAGES
+      export INSTALL=install_macports
+    else
+      echo "Script error"
+      exit 1
     fi
-
-    if [ "${MACPORTS}" = "0" ]; then
-        echo "Installing Shoebot dependencies on MacOSX macports"
-        echo "This is unsupported, let us know if it works"                              
-        sudo port install $MACPORTS_PACKAGES
-    fi
-    hash -r
 }
 
+# get operating system and version
 ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
-
 if [ -f /etc/lsb-release ]; then
     . /etc/lsb-release
     OS=$DISTRIB_ID
@@ -102,25 +94,74 @@ else
     VER=$(uname -r)
 fi
 
-if [ "Debian" = "$OS" ]; then
-    install_debian
-elif [ "LinuxMint" = "$OS" ]; then
-    install_mint
-elif [ "Ubuntu" = "$OS" ]; then
-    install_ubuntu
+if [ "Debian" = "$OS" ] || \
+   [ "Ubuntu" = "$OS" ] || \
+   [ "LinuxMint" = "$OS" ]; then
+    PACKAGE_MANAGER="apt"
+    PACKAGES=$DEB_PACKAGES
+    INSTALL=install_apt
 elif [ "Redhat" = "$OS" ]; then
-    install_redhat
+    PACKAGE_MANAGER="yum"
+    PACKAGES=$REDHAT_PACKAGES
+    INSTALL=install_yum
 elif [ "SuSE" = "$OS" ]; then
-    install_suse
+    PACKAGE_MANAGER="zypper"
+    PACKAGES=$SUSE_PACKAGES
+    INSTALL=install_zypper
 elif [ "Darwin" = "$OS" ]; then
-    install_darwin
-else
+    get_osx_packages_and_installer
+fi
+
+if [[ -z ${INSTALL} ]]; then
     if [ -z "$OS" ]; then
         echo $OS
     fi
-    echo "Shoebot does not directly support $OS/$VER at the moment."
+    echo "Shoebot does not directly support $OS $VER at the moment."
     echo ''
     echo 'Get started by looking at "Add support for another operating system".'
     echo 'in the installation documentation.'
     echo ''
+    exit
 fi
+
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -h|--help)
+       echo "-h|--help             This message."
+       echo "-p|--packagemanager   Show package manager that will be used for install.."
+       echo "-l|--list             Show packages that will be installed."
+       exit
+    ;;
+    -l|--list)
+    LIST_PACKAGES=1
+    shift
+    ;;
+    -p|--packagemanager)
+    SHOW_PACKAGE_MANAGER=1
+    shift
+    ;;
+    *)
+    POSITIONAL+=("$1")
+    shift
+    ;;
+esac
+done
+
+
+if [[ "${SHOW_PACKAGE_MANAGER}" = 1 ]]; then
+    echo $PACKAGE_MANAGER
+fi
+if [[ "${LIST_PACKAGES}" = 1 ]]; then
+    echo $PACKAGES
+fi
+if [[ "${LIST_PACKAGES}${SHOW_PACKAGE_MANAGER}" ]]; then
+    exit
+fi
+
+# Default action, install packages:
+$INSTALL
+hash -r
