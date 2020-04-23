634fa608d5b6726f414453c6cbd8e43dba49c94a
Stuart Axon <stuaxo2@yahoo.com>
2020-04-23 23:11:53 +0100

    Add some tests for PathElements.

--- tests/unittests/test_bezier.py
+++ tests/unittests/test_bezier.py
@@ -1,12 +1,23 @@
+import random
 import unittest
 
+from parameterized import parameterized
+
 from shoebot.core import CairoCanvas, CairoImageSink
-from shoebot.data import BezierPath
+from shoebot.data import (
+    BezierPath,
+    PathElement,
+    CLOSE,
+    MOVETO,
+    RMOVETO,
+    LINETO,
+    RLINETO,
+)
 from shoebot.grammar import NodeBot
 
 
 class TestBezier(unittest.TestCase):
-    # Test the bot API directly.
+    # Test the Bezier API directly.
     def setUp(self):
         sink = CairoImageSink("output-bezier.png")
         canvas = CairoCanvas(sink)
@@ -25,6 +36,52 @@ class TestBezier(unittest.TestCase):
 
         self.assertIsNot(path, copied_path)
         self.assertCountEqual(path, copied_path)
+
+
+class TestPathElement(unittest.TestCase):
+    # Test the Bezier API directly.
+    # TODO:  Add tests for:
+    #  CURVETO, RCURVETO
+    #  ARC, ELLIPSE and None
+    def setUp(self):
+        sink = CairoImageSink("output-bezier-pathelement.png")
+        canvas = CairoCanvas(sink)
+        self.bot = NodeBot(canvas=canvas)
+
+    @parameterized.expand([CLOSE, MOVETO, RMOVETO])
+    def test_xy_no_controlpoints(self, cmd):
+        """
+        PathElement types close, moveto, rmoveto all store an x, y
+        coordinate and all other properties are set to None.
+        """
+        position = random.randint(0, 800), random.randint(0, 800)
+
+        element = PathElement(cmd, *position)
+
+        self.assertEqual(position, (element.x, element.y))
+        self.assertIsNone(element.c1x)
+        self.assertIsNone(element.c1y)
+        self.assertIsNone(element.c2x)
+        self.assertIsNone(element.c2y)
+
+    @parameterized.expand([LINETO, RLINETO])
+    def test_xy_and_one_controlpoints(self, cmd):
+        """
+        PathElement types lineto, rlineto store an x, y and
+        a single control point.
+
+        All other properties are set to None.
+        """
+        # TODO:  Not verified against Nodebox. this test
+        #        reflects current Shoebot behaviour which
+        #        not be correct.
+        position = random.randint(0, 800), random.randint(0, 800)
+
+        element = PathElement(cmd, *position)
+
+        self.assertEqual(position, (element.x, element.y))
+        self.assertEqual(position, (element.c1x, element.c1y))
+        self.assertEqual((element.c2x, element.c2y), position)
 
 
 if __name__ == "__main__":
