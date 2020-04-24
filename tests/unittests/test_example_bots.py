2f766e69f4bdacad7dd4ac23707b173b3a6be059
Stuart Axon <stuaxo2@yahoo.com>
2020-04-24 01:04:22 +0100

    Add lsystem to bots tested.

--- tests/unittests/test_example_bots.py
+++ tests/unittests/test_example_bots.py
@@ -17,17 +17,22 @@ class TestExampleOutput(ShoebotTestCase):
         [
             ("examples/basic/primitives.bot", "basic--primitives"),
             ("examples/libraries/graph/shortest_path.bot", "graph--shortest_path"),
+            ("examples/libraries/lsystem/growing_plant.bot", "lsystem--growing_plant", {"FRAME": 60}),
         ]
     )
-    def test_static_example_bots(self, filename, output_prefix):
+    def test_static_example_bots(self, filename, output_prefix, namespace=None):
         """
         Check non animated example bots render matches the reference images.
         """
         actual_output = self.example_output_dir / f"{output_prefix}-actual.png"
         expected_output = self.example_input_dir / f"{output_prefix}-expected.png"
 
-        self.run_filename(filename, outputfile=actual_output, windowed=self.windowed)
+        # Always run the script, even if the reference image is not present:
+        # this catches more errors and means the output can still be visually inspected.
+        self.run_filename(filename,
+                          outputfile=actual_output, windowed=self.windowed, namespace=namespace)
 
+        self.assertFileExists(expected_output, f"Missing example output image: {expected_output}.")
         self.assertFileSize(actual_output)
         self.assertReferenceImage(actual_output, expected_output)
 
