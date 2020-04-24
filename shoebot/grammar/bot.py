30acb300c7d77c9892080533bf8f59622a81ba9e
Stuart Axon <stuaxo2@yahoo.com>
2020-04-24 16:22:39 +0100

    Always save snapshot, default to shoebot.svg if name can't be found.

--- shoebot/grammar/bot.py
+++ shoebot/grammar/bot.py
@@ -36,37 +36,50 @@ Drawbot and Nodebot are similar grammars, so they both inherit from Bot
 import sys
 import os
 
+from pkg_resources import resource_filename, Requirement
+from glob import glob
+
 from shoebot.core.backend import cairo
-from shoebot.data import BezierPath, EndClip, Color, Text, Variable, \
-                         Image, ClippingPath, \
-                         NUMBER, TEXT, BOOLEAN, BUTTON, \
-                         ShoebotError, \
-                         RGB, HSB, \
-                         CENTER, CORNER, CORNERS
+from shoebot.data import (
+    BezierPath,
+    EndClip,
+    Color,
+    Text,
+    Variable,
+    Image,
+    ClippingPath,
+    NUMBER,
+    TEXT,
+    BOOLEAN,
+    BUTTON,
+    RGB,
+    HSB,
+    CENTER,
+    CORNER,
+    CORNERS,
+)
 
 from .grammar import Grammar
 
-from pkg_resources import resource_filename, Requirement
-from glob import glob
-import random as r
-
-import locale
 import gettext
+import locale
+import random as _random
 
-SBOT_ROOT = resource_filename(Requirement.parse("shoebot"), "")
-APP = 'shoebot'
-DIR = sys.prefix + '/share/shoebot/locale'
-locale.setlocale(locale.LC_ALL, '')
+SHOEBOT_ROOT = resource_filename(Requirement.parse("shoebot"), "")
+APP = "shoebot"
+DIR = sys.prefix + "/share/shoebot/locale"
+
+locale.setlocale(locale.LC_ALL, "")
 gettext.bindtextdomain(APP, DIR)
-# gettext.bindtextdomain(APP)
 gettext.textdomain(APP)
 _ = gettext.gettext
 
 LIB_DIRS = [
-    os.path.join(SBOT_ROOT, 'local', 'share', 'shoebot', 'lib'),
-    os.path.join(SBOT_ROOT, 'lib'),
-    os.path.join(SBOT_ROOT, 'share', 'shoebot', 'lib'),
-    os.path.join(sys.prefix, 'share', 'shoebot', 'lib')]
+    os.path.join(SHOEBOT_ROOT, "local", "share", "shoebot", "lib"),
+    os.path.join(SHOEBOT_ROOT, "lib"),
+    os.path.join(SHOEBOT_ROOT, "share", "shoebot", "lib"),
+    os.path.join(sys.prefix, "share", "shoebot", "lib"),
+]
 for LIB_DIR in LIB_DIRS:
     if os.path.isdir(LIB_DIR):
         sys.path.append(LIB_DIR)
@@ -76,22 +89,23 @@ BOTTOM_LEFT = 2
 
 
 class Bot(Grammar):
-    '''
+    """
     The Parts of the Grammar common to DrawBot, NodeBot and ShoeBot.
-    '''
+    """
+
     RGB = RGB
     HSB = HSB
 
-    LEFT = 'left'
-    RIGHT = 'right'
+    LEFT = "left"
+    RIGHT = "right"
 
     CENTER = CENTER
     CORNER = CORNER
     CORNERS = CORNERS
 
-    LEFT = 'left'
-    RIGHT = 'right'
-    JUSTIFY = 'justify'
+    LEFT = "left"
+    RIGHT = "right"
+    JUSTIFY = "justify"
 
     NUMBER = NUMBER
     TEXT = TEXT
@@ -108,16 +122,16 @@ class Bot(Grammar):
     mousedown = False
 
     # Default key values
-    key = '-'
+    key = "-"
     keycode = 0
     keydown = False
 
     def __init__(self, canvas, namespace=None, vars=None):
-        '''
+        """
         :param canvas: Canvas implementation for output.
         :param namespace: Optionally specify a dict to inject as namespace
         :param vars: Optional dict containing initial values for variables
-        '''
+        """
         Grammar.__init__(self, canvas, namespace=namespace, vars=vars)
         canvas.set_bot(self)
 
@@ -138,17 +152,17 @@ class Bot(Grammar):
                 raise ValueError("Frame must be an integer.")
         else:
             self._frame = 1
-        self._set_initial_defaults() ### TODO Look at these
+        self._set_initial_defaults()  ### TODO Look at these
 
     def _set_initial_defaults(self):
-        '''Set the default values. Called at __init__ and at the end of run(),
+        """Set the default values. Called at __init__ and at the end of run(),
         do that new draw loop iterations don't take up values left over by the
-        previous one.'''
+        previous one."""
         DEFAULT_WIDTH, DEFAULT_HEIGHT = self._canvas.DEFAULT_SIZE
-        self.WIDTH = self._namespace.get('WIDTH', DEFAULT_WIDTH)
-        self.HEIGHT = self._namespace.get('HEIGHT', DEFAULT_WIDTH)
-        if 'WIDTH' in self._namespace or 'HEIGHT' in self._namespace:
-            self.size(w=self._namespace.get('WIDTH'), h=self._namespace.get('HEIGHT'))
+        self.WIDTH = self._namespace.get("WIDTH", DEFAULT_WIDTH)
+        self.HEIGHT = self._namespace.get("HEIGHT", DEFAULT_WIDTH)
+        if "WIDTH" in self._namespace or "HEIGHT" in self._namespace:
+            self.size(w=self._namespace.get("WIDTH"), h=self._namespace.get("HEIGHT"))
 
         self._transformmode = Bot.CENTER
 
@@ -157,38 +171,39 @@ class Bot(Grammar):
             fontsize=16,
             align=Bot.LEFT,
             lineheight=1,
-            fillcolor=self.color(.2),
+            fillcolor=self.color(0.2),
             strokecolor=None,
             strokewidth=1.0,
-            background=self.color(1, 1, 1))
+            background=self.color(1, 1, 1),
+        )
 
     def _set_dynamic_vars(self):
-        self._namespace['FRAME'] = self._frame
+        self._namespace["FRAME"] = self._frame
 
     # Input GUI callbacks
 
     def _mouse_button_down(self, button):
-        '''GUI callback for mouse button down'''
-        self._namespace['mousedown'] = True
+        """GUI callback for mouse button down"""
+        self._namespace["mousedown"] = True
 
     def _mouse_button_up(self, button):
-        '''GUI callback for mouse button up'''
-        self._namespace['mousedown'] = self._input_device.mouse_down
+        """GUI callback for mouse button up"""
+        self._namespace["mousedown"] = self._input_device.mouse_down
 
     def _mouse_pointer_moved(self, x, y):
-        '''GUI callback for mouse moved'''
-        self._namespace['MOUSEX'] = x
-        self._namespace['MOUSEY'] = y
+        """GUI callback for mouse moved"""
+        self._namespace["MOUSEX"] = x
+        self._namespace["MOUSEY"] = y
 
     def _key_pressed(self, key, keycode):
-        '''GUI callback for key pressed'''
-        self._namespace['key'] = key
-        self._namespace['keycode'] = keycode
-        self._namespace['keydown'] = True
+        """GUI callback for key pressed"""
+        self._namespace["key"] = key
+        self._namespace["keycode"] = keycode
+        self._namespace["keydown"] = True
 
     def _key_released(self, key, keycode):
-        '''GUI callback for key released'''
-        self._namespace['keydown'] = self._input_device.key_down
+        """GUI callback for key released"""
+        self._namespace["keydown"] = self._input_device.key_down
 
     # Functions for override #####
 
@@ -203,8 +218,8 @@ class Bot(Grammar):
     # Classes #####
 
     def _makeInstance(self, clazz, args, kwargs):
-        '''Creates an instance of a class defined in this document.
-           This method sets the context of the object to the current context.'''
+        """Creates an instance of a class defined in this document.
+           This method sets the context of the object to the current context."""
         inst = clazz(self, *args, **kwargs)
         return inst
 
@@ -220,17 +235,17 @@ class Bot(Grammar):
         """
         kwargs = dict(kwargs)
 
-        fill = kwargs.get('fill', self._canvas.fillcolor)
+        fill = kwargs.get("fill", self._canvas.fillcolor)
         if not isinstance(fill, Color):
-            fill = Color(fill, mode='rgb', color_range=1)
-        kwargs['fill'] = fill
+            fill = Color(fill, mode="rgb", color_range=1)
+        kwargs["fill"] = fill
 
-        stroke = kwargs.get('stroke', self._canvas.strokecolor)
+        stroke = kwargs.get("stroke", self._canvas.strokecolor)
         if not isinstance(stroke, Color):
-            stroke = Color(stroke, mode='rgb', color_range=1)
-        kwargs['stroke'] = stroke
+            stroke = Color(stroke, mode="rgb", color_range=1)
+        kwargs["stroke"] = stroke
 
-        kwargs['strokewidth'] = kwargs.get('strokewidth', self._canvas.strokewidth)
+        kwargs["strokewidth"] = kwargs.get("strokewidth", self._canvas.strokewidth)
         inst = clazz(self, *args, **kwargs)
         return inst
 
@@ -264,40 +279,42 @@ class Bot(Grammar):
     # Variables #####
 
     def var(self, name, type, default=None, min=0, max=255, value=None, step=None):
-        v = Variable(name, type, default=default, min=min, max=max, value=value, step=step)
+        v = Variable(
+            name, type, default=default, min=min, max=max, value=value, step=step
+        )
         return self._addvar(v)
 
     # Utility #####
 
     def color(self, *args):
-        '''
+        """
         :param args: color in a supported format.
 
         :return: Color object containing the color.
-        '''
+        """
         return self.Color(mode=self.color_mode, color_range=self.color_range, *args)
 
-    choice = r.choice
+    choice = _random.choice
 
     def random(self, v1=None, v2=None):
         # ipsis verbis from Nodebox
         if v1 is not None and v2 is None:
             if isinstance(v1, float):
-                return r.random() * v1
+                return _random.random() * v1
             else:
-                return int(r.random() * v1)
+                return int(_random.random() * v1)
         elif v1 is not None and v2 is not None:
             if isinstance(v1, float) or isinstance(v2, float):
                 start = min(v1, v2)
                 end = max(v1, v2)
-                return start + r.random() * (end - start)
+                return start + _random.random() * (end - start)
             else:
                 start = min(v1, v2)
                 end = max(v1, v2) + 1
-                return int(start + r.random() * (end - start))
+                return int(start + _random.random() * (end - start))
         else:
             # No values means 0.0 -> 1.0
-            return r.random()
+            return _random.random()
 
     def grid(self, cols, rows, colSize=1, rowSize=1, shuffled=False):
         """Returns an iterator that contains coordinate tuples.
@@ -307,12 +324,11 @@ class Bot(Grammar):
                 rect(x,y, 10,10)
         """
         # Taken ipsis verbis from Nodebox
-        from random import shuffle
         rowRange = list(range(int(rows)))
         colRange = list(range(int(cols)))
-        if (shuffled):
-            shuffle(rowRange)
-            shuffle(colRange)
+        if shuffled:
+            _random.shuffle(rowRange)
+            _random.shuffle(colRange)
         for y in rowRange:
             for x in colRange:
                 yield (x * colSize, y * rowSize)
@@ -328,12 +344,12 @@ class Bot(Grammar):
         return glob(path)
 
     def snapshot(self, target=None, defer=None, autonumber=False):
-        '''Save the contents of current surface into a file or cairo surface/context
+        """Save the contents of current surface into a file or cairo surface/context
 
         :param filename: Can be a filename or a Cairo surface.
         :param defer: If true, buffering/threading may be employed however output will not be immediate.
         :param autonumber: If true then a number will be appended to the filename.
-        '''
+        """
         if autonumber:
             file_number = self._frame
         else:
@@ -352,29 +368,29 @@ class Bot(Grammar):
             return
         elif target is None:
             # If nothing specified, use a default filename from the script name
-            script_file = self._namespace.get('__file__')
+            script_file = self._namespace.get("__file__")
             if script_file:
-                target = os.path.splitext(script_file)[0] + '.svg'
-                file_number = True
+                target = os.path.splitext(script_file)[0] + ".svg"
+            else:
+                target = "shoebot.svg"
+            file_number = True
 
-        if target:
-            # snapshot to file, target is a filename
-            if defer is None:
-                defer = True
-            self._canvas.snapshot(target, defer=defer, file_number=file_number)
-        else:
-            raise ShoebotError('No image saved')
+        # snapshot to file, target is a filename
+        if defer is None:
+            defer = True
+        self._canvas.snapshot(target, defer=defer, file_number=file_number)
 
-    def show(self, format='png', as_data=False):
-        '''Returns an Image object of the current surface. Used for displaying
-        output in Jupyter notebooks. Adapted from the cairo-jupyter project.'''
+    def show(self, format="png", as_data=False):
+        """Returns an Image object of the current surface. Used for displaying
+        output in Jupyter notebooks. Adapted from the cairo-jupyter project."""
 
         from io import BytesIO
 
         b = BytesIO()
 
-        if format == 'png':
+        if format == "png":
             from IPython.display import Image
+
             surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, self.WIDTH, self.HEIGHT)
             self.snapshot(surface)
             surface.write_to_png(b)
@@ -384,8 +400,9 @@ class Bot(Grammar):
                 return data
             else:
                 return Image(data)
-        elif format == 'svg':
+        elif format == "svg":
             from IPython.display import SVG
+
             surface = cairo.SVGSurface(b, self.WIDTH, self.HEIGHT)
             surface.finish()
             b.seek(0)
@@ -396,14 +413,14 @@ class Bot(Grammar):
                 return SVG(data)
 
     def ximport(self, libName):
-        '''
+        """
         Import Nodebox libraries.
 
         The libraries get _ctx, which provides
         them with the nodebox API.
 
         :param libName: Library name to import
-        '''
+        """
         # from Nodebox
         lib = __import__(libName)
         self._namespace[libName] = lib
@@ -413,13 +430,13 @@ class Bot(Grammar):
     # Core functions ####
 
     def size(self, w=None, h=None):
-        '''Set the canvas size
+        """Set the canvas size
 
         Only the first call will actually be effective.
 
-        :param w: Width
+        :param w: width
         :param h: height
-        '''
+        """
 
         if not w:
             w = self._canvas.width
@@ -430,17 +447,17 @@ class Bot(Grammar):
 
         # FIXME: Updating in all these places seems a bit hacky
         w, h = self._canvas.set_size((w, h))
-        self._namespace['WIDTH'] = w
-        self._namespace['HEIGHT'] = h
+        self._namespace["WIDTH"] = w
+        self._namespace["HEIGHT"] = h
         self.WIDTH = w  # Added to make evolution example work
         self.HEIGHT = h  # Added to make evolution example work
 
     def speed(self, framerate=None):
-        '''Set animation framerate.
+        """Set animation framerate.
 
         :param framerate: Frames per second to run bot.
         :return: Current framerate of animation.
-        '''
+        """
         if framerate is not None:
             self._speed = framerate
             self._dynamic = True
