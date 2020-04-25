fc7def53d602d44b04d421dfd928150878f5c434
Stuart Axon <stuaxo2@yahoo.com>
2020-04-25 17:20:34 +0100

    Format fixed examples with Black.

--- examples/libraries/making_libraries/daisylib.py
+++ examples/libraries/making_libraries/daisylib.py
@@ -7,29 +7,30 @@
 # which they can use Shoebot/Nodebox commands on.
 #
 #
-# TODO - If we create a 'Shoebot' grammar it should 
+# TODO - If we create a 'Shoebot' grammar it should
 # provide a better mechanism than '_ctx'
 #
 
 from math import sin
 
-def drawdaisy(x, y, color='#fefefe'):
+
+def drawdaisy(x, y, color="#fefefe"):
     """
     Draw a daisy at x, y
     """
     # save location, size etc
     _ctx.push()
-    
+
     # save fill and stroke
-    _fill =_ctx.fill()
+    _fill = _ctx.fill()
     _stroke = _ctx.stroke()
 
     sc = (1.0 / _ctx.HEIGHT) * float(y * 0.5) * 4.0
 
     # draw stalk
     _ctx.strokewidth(sc * 2.0)
-    _ctx.stroke('#3B240B')
-    
+    _ctx.stroke("#3B240B")
+
     _ctx.line(x + (sin(x * 0.1) * 10.0), y + 80, x + sin(_ctx.FRAME * 0.1), y)
 
     # draw flower
@@ -44,7 +45,7 @@ def drawdaisy(x, y, color='#fefefe'):
         _ctx.rect(x, y, 40, 8, 1)
 
     # draw centre
-    _ctx.fill('#F7FE2E')
+    _ctx.fill("#F7FE2E")
     _ctx.ellipse(x + 15, y, 10, 10)
 
     # restore fill and stroke
@@ -53,4 +54,3 @@ def drawdaisy(x, y, color='#fefefe'):
 
     # restore location, size etc
     _ctx.pop()
-
