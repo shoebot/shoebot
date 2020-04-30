88287602b459fea8b35b5db26055d9e5da992adf
Stuart Axon <stuaxo2@yahoo.com>
2020-04-30 01:57:32 +0100

    Add a test that setup.py can load the version from setup.

--- tests/unittests/test_setup.py
+++ tests/unittests/test_setup.py
@@ -2,36 +2,69 @@ import re
 import sys
 import unittest
 
+from importlib import reload
 from pathlib import Path
 from textwrap import dedent
-from unittest.mock import patch, Mock
+from unittest.mock import patch, mock_open, Mock
 
 from tests.unittests.helpers import ShoebotTestCase
 
 sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 
 # Regex from semver.org to recognize if text is a semantic version.
-SEMVER_REGEX = "^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+SEMVER_REGEX = r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
 
 
 class TestSetup(unittest.TestCase):
+    @patch("setuptools.setup", Mock())
     def test_version(self):
         """
         Mock setuptools.setup and find the version parameter passed to it.
 
         Verify that the version is in semantic version format.
         """
-        with patch("setuptools.setup", Mock()) as _setup:
+        import setup
+
+        setup = reload(setup)
+
+        args, kwargs = setup.setup.call_args_list[0]
+
+        version = kwargs.get("version")
+        match = re.match(SEMVER_REGEX, version).groups()
+
+        self.assertIn("version", kwargs)
+        self.assertIsNotNone(match)
+        self.assertRegex(version, SEMVER_REGEX)
+
+    @patch("setuptools.setup", Mock())
+    def test_setup_parses_version_from_version_file(self):
+        """
+        Test that setup loads and parses the version from the
+        file VERSION.
+
+        Mocks open, to do an end to end test using mock data.
+        """
+        expected_version = "1.2.1"
+        example_versionfile_content = dedent(
+            """\
+            name            Shoebot
+            desc            vector graphics scripting application
+            version         1.2.1
+            url             http://shoebot.net
+            packager        Ricardo Lafuente <r@manufacturaindependente.org>
+            """
+        )
+
+        with patch("builtins.open", mock_open(read_data=example_versionfile_content)):
             import setup
+
+            setup = reload(setup)
 
             args, kwargs = setup.setup.call_args_list[0]
 
-            version = kwargs.get("version")
-            match = re.match(SEMVER_REGEX, version).groups()
+            actual_version = kwargs.get("version")
 
-            self.assertIn("version", kwargs)
-            self.assertIsNotNone(match)
-            self.assertRegex(version, SEMVER_REGEX)
+            self.assertEqual(actual_version, expected_version)
 
 
 if __name__ == "__main__":
