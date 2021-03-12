f76104ec9279ee9e6a05450bdd8cfe48ffa2e13f
Stuart Axon <stuaxo2@yahoo.com>
2021-03-12 11:53:18 +0000

    Fixes found when running all examples.

--- lib/colors/__init__.py
+++ lib/colors/__init__.py
@@ -41,6 +41,7 @@ from copy import deepcopy
 from glob import glob
 from math import degrees, radians, sin, cos, atan2, sqrt
 from math import floor
+from operator import attrgetter
 from random import random, choice
 from xml.dom.minidom import parseString
 
@@ -635,7 +636,7 @@ class Color(BaseColor):
     @property
     def is_grey(self):
         return self.r == self.g == self.b
-    
+
     is_gray = is_grey
 
     @property
@@ -1122,78 +1123,77 @@ class ColorList(_list):
         Returns a list with the smallest distance between two neighboring colors.
         The algorithm has a factorial complexity so it may run slow.
         """
-        if len(self) == 0: return ColorList()
+        if not len(self):
+            return ColorList()
 
         # Find the darkest color in the list.
         root = self[0]
-        for clr in self[1:]:
-            if clr.brightness < root.brightness:
-                root = clr
+        for _color in self[1:]:
+            if _color.brightness < root.brightness:
+                root = _color
 
         # Remove the darkest color from the stack,
         # put it in the sorted list as starting element.
-        stack = [clr for clr in self]
+        stack = [*self]
         stack.remove(root)
-        sorted = [root]
+        sorted_colors = [root]
 
         # Now find the color in the stack closest to that color.
         # Take this color from the stack and add it to the sorted list.
         # Now find the color closest to that color, etc.
         while len(stack) > 1:
-            closest, distance = stack[0], stack[0].distance(sorted[-1])
-            for clr in stack[1:]:
-                d = clr.distance(sorted[-1])
+            closest, distance = stack[0], stack[0].distance(sorted_colors[-1])
+            for _color in stack[1:]:
+                d = _color.distance(sorted_colors[-1])
                 if d < distance:
-                    closest, distance = clr, d
+                    closest, distance = _color, d
             stack.remove(closest)
-            sorted.append(closest)
-        sorted.append(stack[0])
+            sorted_colors.append(closest)
+        sorted_colors.append(stack[0])
 
-        if reversed: _list.reverse(sorted)
-        return ColorList(sorted)
+        if reversed:
+            _list.reverse(sorted_colors)
+        return ColorList(sorted_colors)
 
     def _sorted_copy(self, comparison, reversed=False):
         """
         Returns a sorted copy with the colors arranged according to the given comparison.
         """
-        sorted = self.copy()
-        _list.sort(sorted, comparison)
-        if reversed:
-            _list.reverse(sorted)
-        return sorted
+        sorted_colors = ColorList(*sorted(self.copy(), key=comparison, reverse=reversed))
+        return sorted_colors
 
     def sort_by_hue(self, reversed=False):
-        return self._sorted_copy(lambda a, b: int(a.h < b.h) * 2 - 1, reversed)
+        return self._sorted_copy(attrgetter("h"), reversed)
 
     def sort_by_saturation(self, reversed=False):
-        return self._sorted_copy(lambda a, b: int(a.s < b.s) * 2 - 1, reversed)
+        return self._sorted_copy(attrgetter("s"), reversed)
 
     def sort_by_brightness(self, reversed=False):
-        return self._sorted_copy(lambda a, b: int(a.brightness < b.brightness) * 2 - 1, reversed)
+        return self._sorted_copy(attrgetter("brightness"), reversed)
 
     def sort_by_red(self, reversed=False):
-        return self._sorted_copy(lambda a, b: int(a.r < b.r) * 2 - 1, reversed)
+        return self._sorted_copy(attrgetter("r"), reversed)
 
     def sort_by_green(self, reversed=False):
-        return self._sorted_copy(lambda a, b: int(a.g < b.g) * 2 - 1, reversed)
+        return self._sorted_copy(attrgetter("g"), reversed)
 
     def sort_by_blue(self, reversed=False):
-        return self._sorted_copy(lambda a, b: int(a.b < b.b) * 2 - 1, reversed)
+        return self._sorted_copy(attrgetter("b"), reversed)
 
     def sort_by_alpha(self, reversed=False):
-        return self._sorted_copy(lambda a, b: int(a.a < b.a) * 2 - 1, reversed)
+        return self._sorted_copy(attrgetter("a"), reversed)
 
     def sort_by_cyan(self, reversed=False):
-        return self._sorted_copy(lambda a, b: int(a.c < b.c) * 2 - 1, reversed)
+        return self._sorted_copy(attrgetter("c"), reversed)
 
     def sort_by_magenta(self, reversed=False):
-        return self._sorted_copy(lambda a, b: int(a.m < b.m) * 2 - 1, reversed)
+        return self._sorted_copy(attrgetter("m"), reversed)
 
     def sort_by_yellow(self, reversed=False):
-        return self._sorted_copy(lambda a, b: int(a.y < b.y) * 2 - 1, reversed)
+        return self._sorted_copy(attrgetter("y"), reversed)
 
     def sort_by_black(self, reversed=False):
-        return self._sorted_copy(lambda a, b: int(a.k < b.k) * 2 - 1, reversed)
+        return self._sorted_copy(attrgetter("k"), reversed)
 
     def sort(self, comparison="hue", reversed=False):
 
@@ -1203,8 +1203,7 @@ class ColorList(_list):
         since colors need to be represented in 2 or 3 dimensions.
 
         """
-
-        return getattr(self, "sort_by_" + comparison)(reversed)
+        return getattr(self, f"sort_by_{comparison}")(reversed)
 
     def cluster_sort(self, cmp1="hue", cmp2="brightness", reversed=False, n=12):
         """
@@ -1215,18 +1214,22 @@ class ColorList(_list):
         n is used rather to slice up the cmp1 property of the colors,
         e.g. cmp1=brightness and n=3 will cluster colors by brightness >= 0.66, 0.33, 0.0
         """
-        sorted = self.sort(cmp1)
+        sorted_colors = self.sort(cmp1)
         clusters = ColorList()
 
         d = 1.0
         i = 0
-        for j in _range(len(sorted)):
-            if getattr(sorted[j], cmp1) < d:
-                clusters.extend(sorted[i:j].sort(cmp2))
+        for j in _range(len(sorted_colors)):
+            if getattr(sorted_colors[j], cmp1) < d:
+                if not sorted_colors[i: j]:
+                    continue
+                clusters.extend(sorted_colors[i:j].sort(cmp2))
                 d -= 1.0 / n
                 i = j
-        clusters.extend(sorted[i:].sort(cmp2))
-        if reversed: _list.reverse(clusters)
+        clusters.extend(sorted_colors[i:].sort(cmp2))
+        if reversed:
+            #_list.reverse(clusters)
+            clusters.reverse()
         return clusters
 
     cluster = clustersort = cluster_sort
@@ -1351,10 +1354,10 @@ class ColorList(_list):
     # single Color objects can be added with +,
     # and * equals the repeat() method.
 
-    def __getslice__(self, i, j):
-        j = min(len(self), j)
-        n = min(len(self), j - i)
-        return colorlist([self[i + k] for k in _range(n)])
+    def __getitem__(self, *args):
+        if isinstance(args[0], slice):
+            return ColorList(super().__getitem__(*args))
+        return super().__getitem__(*args)
 
     def __add__(self, clr):
         if isinstance(clr, BaseColor):
@@ -1818,8 +1821,8 @@ class Gradient(ColorList):
 
         # Chop into left half and right half.
         # Make sure their ending and beginning match colors.
-        left = colors[:len(colors) / 2]
-        right = colors[len(colors) / 2:]
+        left = colors[:len(colors) // 2]
+        right = colors[len(colors) // 2:]
         left.append(right[0])
         right.insert(0, left[-1])
 
