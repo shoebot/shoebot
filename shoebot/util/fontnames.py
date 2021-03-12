ba0cd6b9a0a71caed3e852e63b7c9a4f9dfad20a
Ricardo Lafuente <r@manufacturaindependente.org>
2021-03-12 10:08:47 +0000

    Improve fontnames() based on feedback

--- shoebot/util/fontnames.py
+++ shoebot/util/fontnames.py
@@ -5,13 +5,16 @@ from gi.repository import PangoCairo
 # https://developer.gnome.org/pygtk/stable/index.html
 
 def fontnames():
-    fontstrings = []
+    """Returns a list of available Pango font names. Names are returned in the
+    Pango font string format, e.g. "Inconsolata Bold"."""
+    names = []
     families = PangoCairo.font_map_get_default().list_families()
-
     for family in families:
-        for face in family.list_faces():
-            # FIXME: Ensure this is the correct way to make a font name
-            fontstrings.append(family.get_name() + ' ' + face.get_face_name())
-            # description = face.describe()
+        # FIXME: Ensure this is the correct way to make a font name
+        facenames = ['{} {}'.format(family.get_name(), face.get_face_name()) for face in family.list_faces()]
+        names.extend(facenames)
+        # description = face.describe()
+    return names
 
-    return fontstrings
+if __name__ == "__main__":
+    print(fontnames())
