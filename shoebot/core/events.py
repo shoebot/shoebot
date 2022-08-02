408422fa058aa903120c35ad61666b7d8991e8a3
Stuart Axon <stu.axon@gmail.com>
2022-08-02 20:40:15 +0100

    collections.Mapping moved to collections.abc.Mapping

--- shoebot/core/events.py
+++ shoebot/core/events.py
@@ -13,7 +13,7 @@ channel = pubsub.subscribe("shoebot")
 def namedtuple_with_defaults(typename, field_names, default_values=()):
     T = collections.namedtuple(typename, field_names)
     T.__new__.__defaults__ = (None,) * len(T._fields)
-    if isinstance(default_values, collections.Mapping):
+    if isinstance(default_values, collections.abc.Mapping):
         prototype = T(**default_values)
     else:
         prototype = T(*default_values)
