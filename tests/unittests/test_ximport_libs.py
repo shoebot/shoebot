2b2718581356e2317c47f36cb058b6c2253f1081
Stuart Axon <stu.axon@gmail.com>
2022-10-13 00:24:11 +0100

    Add very basic test for ximport:  ximport all the libraries (skips any that need numpy, opencv if these are not present).

---
+++ tests/unittests/test_ximport_libs.py
@@ -0,0 +1,43 @@
+import unittest
+from math import radians
+from unittest import skip
+from unittest.mock import patch
+
+from parameterized import parameterized
+
+from tests.unittests.helpers import ShoebotTestCase
+from tests.unittests.helpers import test_as_bot
+
+class TestXImportLibs(ShoebotTestCase):
+    @parameterized.expand(
+        [
+	   ("beziereditor",),
+           ("boids",),
+           ("colors",),
+           ("cornu",),
+           ("database",),
+           ("graph",),
+           ("lsystem",),
+           ("photobot",),
+           ("sbaudio", "numpy"),
+           ("sbopencv", "opencv", "numpy"),
+           ("sbvideo", "opencv", "numpy"),
+           ("supershape",),
+           ("svg",),
+           ("tuio",),
+        ],
+    )
+    @test_as_bot()
+    def test_path_commands(self, lib, *requires):
+        """Attempt to ximport each of the libraries shoebot provides."""
+        for required in requires:
+            try:
+                __import__(required)
+            except ImportError as e:
+                self.skipTest(f"Skipping Test, required library not found: {e}")
+
+        ximport(lib)
+
+
+if __name__ == "__main__":
+    unittest.main(buffer=False)
